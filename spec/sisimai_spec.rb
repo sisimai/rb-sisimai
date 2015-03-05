aa0f7f9acba88e1b7a063cbf2455622886256d46
azumakuniyuki <azumakuniyuki@gmail.com>
2015-03-05 18:10:30 +0900

    Implement test code for the added methods

--- spec/sisimai_spec.rb
+++ spec/sisimai_spec.rb
@@ -5,7 +5,15 @@ describe Sisimai do
     expect(Sisimai::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'does version() method' do
+    expect(Sisimai.version()).to eq(Sisimai::VERSION)
+  end
+
+  it 'does sysname() method' do
+    expect(Sisimai.sysname()).to eq('bouncehammer')
+  end
+
+  it 'does libname() method' do
+    expect(Sisimai.libname()).to eq('Sisimai')
   end
 end
