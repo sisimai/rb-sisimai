858418c74d4eec76ed0106aa44b50e170d0ec214
azumakuniyuki <azumakuniyuki@gmail.com>
2015-03-06 14:54:11 +0900

    class << self

--- lib/sisimai.rb
+++ lib/sisimai.rb
@@ -2,16 +2,10 @@ require "sisimai/version"
 
 module Sisimai
   # Imported from p5-Sisimail/lib/Sisimai.pm
-  def self.version
-    return Sisimai::VERSION
-  end
-
-  def self.sysname
-    return 'bouncehammer'
-  end
-
-  def self.libname
-    return 'Sisimai'
+  class << self
+    def version(); return Sisimai::VERSION; end
+    def sysname(); return 'bouncehammer';   end
+    def libname(); return 'Sisimai';        end
   end
 
 end
