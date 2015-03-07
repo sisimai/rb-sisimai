f38fcfd8d3a7d133d640162bd7b9c3b352c7b512
azumakuniyuki <azumakuniyuki@gmail.com>
2015-03-07 20:30:13 +0900

    Implement test code for is_8bit() and sweep() in Sisimai::String

--- spec/sisimai-string_spec.rb
+++ spec/sisimai-string_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'sisimai/string'
 
 describe 'Sisimai::String' do
-  describe 'Sisimai::String.token() methods' do
+  describe 'Sisimai::String.token() method' do
     s = 'envelope-sender@example.jp';
     r = 'envelope-recipient@example.org';
     t = '239aa35547613b2fa94f40c7f35f4394e99fdd88';
@@ -32,7 +32,47 @@ describe 'Sisimai::String' do
         expect { Sisimai::String.token(s,r) }.to raise_error(ArgumentError)
       end
     end
-
   end
+
+  describe 'Sisimai::String.is_8bit() method' do
+    it 'is_8bit(8) returns 8' do
+      expect(Sisimai::String.is_8bit(8)).to eq 8
+    end
+
+    it 'is_8bit(neko) returns false' do
+      expect(Sisimai::String.is_8bit('neko')).to be_false
+
+    end
+    it 'is_8bit(日本語) returns true' do
+      expect(Sisimai::String.is_8bit('日本語')).to be_true
+    end
+
+    context 'Errors from the method' do
+      it 'is_8bit() raise an error: ArgumentError' do
+        expect { Sisimai::String.is_8bit() }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe 'Sisimai::String.sweep() method' do
+    it 'sweep(nil) returns nil' do
+      expect(Sisimai::String.sweep(nil)).to be_nil
+    end
+
+    it 'sweep(" neko cat ") returns "neko cat"' do
+      expect(Sisimai::String.sweep(' neko cat ')).to eq 'neko cat'
+    end
+
+    it 'sweep(" nyaa   !!") returns "nyaa !!"' do
+      expect(Sisimai::String.sweep(' nyaa   !!')).to eq 'nyaa !!'
+    end
+
+    context 'Errors from the method' do
+      it 'sweep() raise an error: ArgumentError' do
+        expect { Sisimai::String.sweep() }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
 end
 
