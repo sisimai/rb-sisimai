646c1a2e8df32b9215b8f183876109e861867607
azumakuniyuki <azumakuniyuki@gmail.com>
2015-11-13 20:47:19 +0900

    Implement Sisimai::String.EOM() method

--- lib/sisimai/string.rb
+++ lib/sisimai/string.rb
@@ -3,14 +3,22 @@ require 'digest/sha1'
 module Sisimai::String
   # Imported from p5-Sisimail/lib/Sisimai/String.pm
   class << self
+
+    # End of email message as a sentinel for parsing bounce messages
+    # @private
+    # @return   [String] Fixed length string like a constant
+    def EOM()
+      return '__END_OF_EMAIL_MESSAGE__';
+    end
+
+    # Create message token from addresser and recipient
+    # @param  [String]  addr1 Sender address
+    # @param  [String]  addr2 Recipient address
+    # @param  [Integer] epoch Machine time of the email bounce
+    # @return [String]        Message token(MD5 hex digest)
+    # @return [String]        Blank/failed to create token
+    # @see    http://en.wikipedia.org/wiki/ASCII
     def token( addr1, addr2, epoch )
-      # @Description  Create message token from addresser and recipient
-      # @Param        (String) Sender address
-      # @Param        (String) Recipient address
-      # @Param        (Integer) Machine time of the email bounce
-      # @Return       (String) Message token(MD5 hex digest)
-      #               (String) Blank/failed to create token
-      # @See          http://en.wikipedia.org/wiki/ASCII
       return '' unless addr1.kind_of?(String)
       return '' unless addr2.kind_of?(String)
       return '' unless epoch.is_a?(Integer)
@@ -21,20 +29,22 @@ module Sisimai::String
       )
     end
 
+    # The argument is 8-bit text or not
+    # @param    [String] argvs  Any string to be checked
+    # @return   [True,False]    true:  ASCII Characters only
+    #                           false: Including 8-bit character
     def is_8bit( argvs )
-      # @Description  8bit text or not
-      # @Param <ref>  (String) String
-      # @Return       false = ASCII Characters only
-      #               true  = Including 8bit character
       return argvs unless argvs.kind_of?(String)
       return true  unless argvs =~ /\A[\x00-\x7f]+\z/
       return false
     end
 
+    # Clean the string out
+    # @param    [String] argvs  String to be cleaned
+    # @return   [Scalar]        Cleaned out string
+    # @example  Clean up text
+    #   sweep('  neko ') #=> 'neko'
     def sweep( argvs )
-      # @Description  Clean the string out
-      # @Param <ref>  (String) String
-      # @Return       (String) String cleaned out
       return argvs unless argvs.kind_of?(String)
 
       argvs = argvs.chomp
