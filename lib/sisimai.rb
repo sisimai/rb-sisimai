885ecbb18123dd2d1d98028cc0fd2df3256df824
azumakuniyuki <azumakuniyuki@gmail.com>
2015-11-13 20:48:14 +0900

    Implement make() and dump() methods as a skelton

--- lib/sisimai.rb
+++ lib/sisimai.rb
@@ -6,6 +6,19 @@ module Sisimai
     def version(); return Sisimai::VERSION; end
     def sysname(); return 'bouncehammer';   end
     def libname(); return 'Sisimai';        end
+
+    # Wrapper method for parsing mailbox/maidir
+    # @param    [String] mbox   Path to mbox or Maildir/
+    # @return   [Array]         Parsed objects
+    # @return   [nil]           nil if the argument was wrong or an empty array
+    def make(path)
+    end
+
+    # Wrapper method to parse mailbox/Maildir and dump as JSON
+    # @param        [String] mbox Path to mbox or Maildir/
+    # @return       [String] Parsed data as JSON text
+    def dump(path)
+    end
   end
 
 end
