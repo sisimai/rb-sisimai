0f3e45aab3f552d070b1be7788d5427463dff92a
azumakuniyuki <azumakuniyuki@gmail.com>
2015-11-20 21:47:17 +0900

    Improved test code for Sisimai::Rhost.list

--- spec/sisimai-rhost_spec.rb
+++ spec/sisimai-rhost_spec.rb
@@ -1,28 +1,59 @@
 require 'spec_helper'
 require 'sisimai/rhost'
 
-describe 'Sisimai::Rhost' do
-  describe 'Sisimai::Rhost.list() method' do
-    v = Sisimai::Rhost.list()
-    it 'list() returns a list' do
-      expect(v.kind_of?(Array)).to be true
+describe Sisimai::Rhost do
+  cn = Sisimai::Rhost
+  describe '.list' do
+    context '()' do
+      v = cn.list
+      it 'returns Array' do
+        expect(v.kind_of?(Array)).to be true
+      end
+      v.each do |e|
+        describe e do
+          it('is a String')       { expect(e.kind_of?(String)).to be true }
+          it('is valid hostname') { expect(e).to match(/\A[-.a-z0-9]+\z/) }
+        end
+      end
+    end
+
+    describe 'Wrong Number of Arguments' do
+      context '(nil)' do
+        it('raises ArgumentError') { expect { cn.list(nil) }.to raise_error(ArgumentError) }
+      end
+      context '(nil,nil)' do
+        it('raises ArgumentError') { expect { cn.list(nil, nil) }.to raise_error(ArgumentError) }
+      end
     end
   end
 
-  describe 'Sisimai::Rhost.match() method' do
-    it 'match(aspmx.l.google.com) returns True' do
-      expect(Sisimai::Rhost.match('aspmx.l.google.com')).to be true
+  describe '.match' do
+    describe 'Valid Argument String' do
+      v = ['aspmx.l.google.com']
+      v.each do |e|
+        context "(#{e})" do
+          it('returns true') { expect(cn.match(e)).to be true }
+        end
+      end
+      context 'example.jp' do
+        it('returns false') { expect(cn.match('example.jp')).to be false }
+      end
     end
-    it 'match(example.jp) returns False' do
-      expect(Sisimai::Rhost.match('example.jp')).to be false
+
+    describe 'Wrong Number of Arguments' do
+      context '(nil,nil)' do
+        it('raises ArgumentError') { expect { cn.match(nil, nil) }.to raise_error(ArgumentError) }
+      end
     end
   end
 
-  describe 'Sisimai::Rhost.get() method' do
+  describe 'get' do
     v = nil
-    it 'get(v) returns reason text' do
-      pending 'Tests will be added after implementation of Sisimai::Data class'
-      expect(Sisimai::Rhost.get(v)).not_to be nil
+    context '(nil)' do
+      it 'returns reason text' do
+        pending 'Tests will be added after implementation of Sisimai::Data class'
+        expect(cn.get(v)).not_to be nil
+      end
     end
   end
 end
