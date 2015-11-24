cf63cf37e26f90326aee63ebb40b092e16513747
azumakuniyuki <azumakuniyuki@gmail.com>
2015-11-24 21:04:32 +0900

    Tiny improvement: use subject block

--- spec/sisimai_spec.rb
+++ spec/sisimai_spec.rb
@@ -2,21 +2,30 @@ require 'spec_helper'
 
 describe Sisimai do
   cn = Sisimai
-  v  = cn::VERSION
+  bH = 'bounceHammer'
+
   describe 'Sisimai::VERSION' do
-    it('has a version number')   { expect(cn::VERSION).not_to be nil }
-    it('version number is ' + v) { expect(cn::VERSION).to eq v }
+    subject { Sisimai::VERSION }
+    it('returns version') { is_expected.not_to be nil }
+    it('returns String' ) { is_expected.to be_a(String) }
+    it('matches X.Y.Z'  ) { is_expected.to match(/\A\d[.]\d+[.]\d+/) }
   end
 
   describe '.version' do
-    it('returns ' + v) { expect(cn.version).to eq v }
+    subject { cn.version }
+    it('is String') { is_expected.to be_a(String) }
+    it('is ' + Sisimai::VERSION) { is_expected.to eq Sisimai::VERSION }
   end
 
   describe '.sysname' do
-    it('returns "bouncehammer"') { expect(cn.sysname).to eq 'bouncehammer' }
+    subject { cn.sysname }
+    it('is String')     { is_expected.to be_a(String) }
+    it('returns ' + bH) { is_expected.to match(/bounceHammer/i) }
   end
 
   describe '.libname' do
-    it('returns ' + cn.libname) { expect(cn.libname).to eq 'Sisimai' }
+    subject { cn.libname }
+    it('is String')       { is_expected.to be_a(String) }
+    it('returns Sisimai') { expect(cn.libname).to eq 'Sisimai' }
   end
 end
