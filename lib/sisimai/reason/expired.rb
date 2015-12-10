43035aceea1271d7e901adad1e2b2ee5a6fe847b
azumakuniyuki <azumakuniyuki@gmail.com>
2015-12-11 02:49:41 +0900

    Import Sisimai::Reason::Expired from p5-Sisimai: issue #6

---
+++ lib/sisimai/reason/expired.rb
@@ -0,0 +1,46 @@
+module Sisimai
+  module Reason
+    # Sisimai::Reason::Expired checks the bounce reason is "expired" or not.
+    # This class is called only Sisimai::Reason class.
+    #
+    # This is the error that delivery time has expired due to connection failure
+    # or network error and the message you sent has been in the queue for long 
+    # time.
+    module Expired
+      # Imported from p5-Sisimail/lib/Sisimai/Reason/Expired.pm
+      class << self
+        def text; return 'expired'; end
+
+        # Try to match that the given text and regular expressions
+        # @param    [String] argv1  String to be matched with regular expressions
+        # @return   [True,False]    false: Did not match
+        #                           true: Matched
+        def match(argv1)
+          return nil unless argv1
+          regex = %r{(?:
+             connection[ ]timed[ ]out
+            |could[ ]not[ ]find[ ]a[ ]gateway[ ]for
+            |delivery[ ]time[ ]expired
+            |giving[ ]up[ ]on
+            |it[ ]has[ ]not[ ]been[ ]collected[ ]after
+            |message[ ]expired[ ]after[ ]sitting[ ]in[ ]queue[ ]for
+            |Message[ ]timed[ ]out
+            |retry[ ]time[ ]not[ ]reached[ ]for[ ]any[ ]host[ ]after[ ]a[ ]long[ ]failure[ ]period
+            |server[ ]did[ ]not[ ]respond
+            |this[ ]message[ ]has[ ]been[ ]in[ ]the[ ]queue[ ]too[ ]long
+            |was[ ]not[ ]reachable[ ]within[ ]the[ ]allowed[ ]queue[ ]period
+            )
+          }ix
+
+          return true if argv1 =~ regex
+          return false
+        end
+
+        def true; return nil; end
+
+      end
+    end
+  end
+end
+
+
