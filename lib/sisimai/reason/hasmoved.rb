0c0e5ada5b52350b91c5d11f877323d9bd6deb40
azumakuniyuki <azumakuniyuki@gmail.com>
2015-12-11 02:49:41 +0900

    Import Sisimai::Reason::HasMoved from p5-Sisimai: issue #6

---
+++ lib/sisimai/reason/hasmoved.rb
@@ -0,0 +1,45 @@
+module Sisimai
+  module Reason
+    # Sisimai::Reason::HasMoved checks the bounce reason is "hasmoved" or not.
+    # This class is called only Sisimai::Reason class.
+    #
+    # This is the error that a user's mailbox has moved (and is not forwarded 
+    # automatically). Sisimai will set "hasmoved" to the reason of email bounce
+    # if the value of Status: field in a bounce email is "5.1.6".
+    module HasMoved
+      # Imported from p5-Sisimail/lib/Sisimai/Reason/HasMoved.pm
+      class << self
+        def text; return 'hasmoved'; end
+
+        # Try to match that the given text and regular expressions
+        # @param    [String] argv1  String to be matched with regular expressions
+        # @return   [True,False]    false: Did not match
+        #                           true: Matched
+        def match(argv1)
+          return nil unless argv1
+          regex = %r/address[ ].+[ ]has[ ]been[ ]replaced[ ]by[ ]/ix
+
+          return true if argv1 =~ regex
+          return false
+        end
+
+        # Whether the address has moved or not
+        # @param    [Sisimai::Data] argvs   Object to be detected the reason
+        # @return   [True,False]            true: The address has moved
+        #                                   false: Has not moved
+        # @see http://www.ietf.org/rfc/rfc2822.txt
+        def true(argvs)
+          return nil unless argvs
+          return nil unless argvs.is_a? Sisimai::Data
+          return true if argvs.reason == self.text
+          return true if self.match(argvs.diagnosticcode)
+          return false
+        end
+
+      end
+    end
+  end
+end
+
+
+
