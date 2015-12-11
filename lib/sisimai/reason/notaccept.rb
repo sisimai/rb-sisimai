2380d6ae610628f7214a17a38de38b954b9b38a0
azumakuniyuki <azumakuniyuki@gmail.com>
2015-12-11 13:55:21 +0900

    Call class method with the class name

--- lib/sisimai/reason/notaccept.rb
+++ lib/sisimai/reason/notaccept.rb
@@ -1,5 +1,13 @@
 module Sisimai
   module Reason
+    # Sisimai::Reason::NotAccept checks the bounce reason is "notaccept" or not.
+    # This class is called only Sisimai::Reason class.
+    #
+    # This is the error that a destination mail server does ( or can ) not accept
+    # any email. In many case, the server is high load or under the maintenance.
+    # Sisimai will set "notaccept" to the reason of email bounce if the value of
+    # Status: field in a bounce email is "5.3.2" or the value of SMTP reply code
+    # is 556.
     module NotAccept
       # Imported from p5-Sisimail/lib/Sisimai/Reason/NotAccept.pm
       class << self
@@ -27,7 +35,7 @@ module Sisimai
         def true(argvs)
           return nil unless argvs
           return nil unless argvs.is_a? Sisimai::Data
-          return true if argvs.reason == self.text
+          return true if argvs.reason == Sisimai::Reason::NotAccept.text
 
           diagnostic = argvs.diagnosticcode || ''
           v = false
@@ -39,9 +47,10 @@ module Sisimai
             # Check the value of Diagnosic-Code: header with patterns
             if argvs.smtpcommand == 'MAIL'
               # Matched with a pattern in this class
-              v = true if self.match(diagnostic)
+              v = true if Sisimai::Reason::NotAccept.match(diagnostic)
             end
           end
+
           return v
         end
 
