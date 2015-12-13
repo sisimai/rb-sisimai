f06ec8cbb9ede13a44219c34268f832408c0f871
azumakuniyuki <azumakuniyuki@gmail.com>
2015-12-13 17:32:11 +0900

    Import Sisimai::Data::YAML from p5-Sisimai, issue #6

---
+++ lib/sisimai/data/yaml.rb
@@ -0,0 +1,32 @@
+module Sisimai
+  class Data
+    # Sisimai::Data::YAML dumps parsed data object as a YAML format. This class
+    # and method should be called from the parent object "Sisimai::Data".
+    module YAML
+      # Imported from p5-Sisimail/lib/Sisimai/Data/YAML.pm
+      class << self
+        require 'yaml'
+
+        # Data dumper(YAML)
+        # @param    [Sisimai::Data] argvs   Object
+        # @return   [String, Undef]         Dumped data or Undef if the argument
+        #                                   is missing
+        def dump(argvs)
+          return nil unless argvs
+          return nil unless argvs.is_a? Sisimai::Data
+
+          damneddata = argvs.damn
+          yamlstring = nil
+
+          begin
+            yamlstring = ::YAML.dump(damneddata)
+          rescue
+            warn '***warning: Failed to YAML.dump'
+          end
+
+          return yamlstring
+        end
+      end
+    end
+  end
+end
