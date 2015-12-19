257120336f3a5d2c516cc209f6b2b05c0fa34a07
azumakuniyuki <azumakuniyuki@gmail.com>
2015-12-19 12:09:16 +0900

    Add value table of parsed results for Sisimai::MTA::McAfee, issue #7

--- spec/sisimai/mta-children_spec.rb
+++ spec/sisimai/mta-children_spec.rb
@@ -52,6 +52,11 @@ MTAChildren = {
     '27' => { 'status' => %r/\A5[.]0[.]\d+\z/, 'reason' => %r/blocked/ },
     '28' => { 'status' => %r/\A5[.]0[.]\d+\z/, 'reason' => %r/mailererror/ },
   },
+  'McAfee' => {
+    '01' => { 'status' => %r/\A5[.]0[.]\d+\z/, 'reason' => %r/userunknown/ },
+    '02' => { 'status' => %r/\A5[.]1[.]1\z/, 'reason' => %r/userunknown/ },
+    '03' => { 'status' => %r/\A5[.]1[.]1\z/, 'reason' => %r/userunknown/ },
+  },
   'OpenSMTPD' => {
     '01' => { 'status' => %r/\A5[.]1[.]1\z/, 'reason' => %r/userunknown/ },
     '02' => { 'status' => %r/\A5[.][12][.][12]\z/, 'reason' => %r/(?:userunknown|mailboxfull)/ },
