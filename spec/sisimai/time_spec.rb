08d743fa582ca9b2775fe6aeac809857ae24c728
azumakuniyuki <azumakuniyuki@gmail.com>
2016-01-11 14:41:55 +0900

    Add test code for Sisimai::Time#to_json: issue #12

--- spec/sisimai/time_spec.rb
+++ spec/sisimai/time_spec.rb
@@ -7,4 +7,9 @@ describe Sisimai::Time do
   describe '.new' do
     it('returns Sisimai::Time object') { expect(to).to be_a Sisimai::Time }
   end
+
+  describe '.to_json' do
+    it('returns Integer') { expect(to.to_json).to be_a Integer }
+    it('returns machine time') { expect(to.to_json).to be to.to_time.to_i }
+  end
 end
