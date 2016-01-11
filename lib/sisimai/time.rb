51a0a3480a46e0eef5237ccbbaf64149e6729e85
azumakuniyuki <azumakuniyuki@gmail.com>
2016-01-11 14:41:16 +0900

    Implement Sisimai::Time#to_json method: issue 12

--- lib/sisimai/time.rb
+++ lib/sisimai/time.rb
@@ -3,5 +3,8 @@ module Sisimai
   # Sisimai::Time is a child class of Date for Sisimai::Data.
   class Time < DateTime
     # Imported from p5-Sisimail/lib/Sisimai/Time.pm
+    def to_json
+      return self.to_time.to_i
+    end
   end
 end
