fe9b2915fbff9ffad1f75432718b604a11309955
azumakuniyuki <azumakuniyuki@gmail.com>
2016-01-15 00:22:16 +0900

    Remove "cn" variable

--- spec/sisimai/mta_spec.rb
+++ spec/sisimai/mta_spec.rb
@@ -2,15 +2,13 @@ require 'spec_helper'
 require 'sisimai/mta'
 
 describe Sisimai::MTA do
-  cn = Sisimai::MTA
-
   describe '.INDICATORS' do
-    it('returns Hash') { expect(cn.INDICATORS).to be_a Hash }
+    it('returns Hash') { expect(Sisimai::MTA.INDICATORS).to be_a Hash }
   end
   describe '.DELIVERYSTATUS' do
-    it('returns Hash') { expect(cn.DELIVERYSTATUS).to be_a Hash }
+    it('returns Hash') { expect(Sisimai::MTA.DELIVERYSTATUS).to be_a Hash }
   end
   describe '.index' do
-    it('returns Array') { expect(cn.index).to be_a Array }
+    it('returns Array') { expect(Sisimai::MTA.index).to be_a Array }
   end
 end
