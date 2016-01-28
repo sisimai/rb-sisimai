ec67bb9424c79907b7a9e4ba434fd9e065daf3d5
azumakuniyuki <azumakuniyuki@gmail.com>
2016-01-28 15:07:17 +0900

    Add configuration for Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear!
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
+  SimpleCov.start 'test_frameworks'
+end
