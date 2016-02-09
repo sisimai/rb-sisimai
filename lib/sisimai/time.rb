94ab33cd0b2acd2470f088829eac76fe4788513f
azumakuniyuki <azumakuniyuki@gmail.com>
2016-02-10 01:26:25 +0900

    Add definition of the argument at Sisimai::Time#to_json method

--- lib/sisimai/time.rb
+++ lib/sisimai/time.rb
@@ -4,7 +4,7 @@ module Sisimai
   # Sisimai::Time is a child class of Date for Sisimai::Data.
   class Time < DateTime
     # Imported from p5-Sisimail/lib/Sisimai/Time.pm
-    def to_json
+    def to_json(*)
       return self.to_time.to_i
     end
   end
