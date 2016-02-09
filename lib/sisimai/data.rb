b9e706bc3bdd5114a8ecd75dad677646ae86c9a8
azumakuniyuki <azumakuniyuki@gmail.com>
2016-02-10 02:16:51 +0900

    Improved Sisimai::Data#damn, set alias to "damn" as "to_hash", issue #42

--- lib/sisimai/data.rb
+++ lib/sisimai/data.rb
@@ -361,28 +361,17 @@ module Sisimai
     # Convert from object to hash reference
     # @return   [Hash] Data in Hash reference
     def damn
-      data = nil
-      begin
-        v = {}
-        stringdata = %w[
-          token lhost rhost listid alias reason subject messageid smtpagent
-          smtpcommand destination diagnosticcode senderdomain deliverystatus
-          timezoneoffset feedbacktype diagnostictype action replycode softbounce
-        ]
-
-        stringdata.each do |e|
-          # Copy string data
-          v[e] = self.send(e) || ''
-        end
-        v['addresser'] = self.addresser.address
-        v['recipient'] = self.recipient.address
-        v['timestamp'] = self.timestamp.to_time.to_i
-        data = v
-      rescue
-        warn '***warning: Failed to damn()'
+      data = {}
+      @@rwaccessors.each do |e|
+        next if e.to_s =~ /(?:addresser|recipient|timestamp)/
+        data[e.to_s] = self.send(e) || ''
       end
+      data['addresser'] = self.addresser.address
+      data['recipient'] = self.recipient.address
+      data['timestamp'] = self.timestamp.to_time.to_i
       return data
     end
+    alias :to_hash :damn
 
     # Data dumper
     # @param    [String] type   Data format: json, yaml
@@ -402,16 +391,10 @@ module Sisimai
       return dumpeddata
     end
 
-    def to_hash
-      data = {}
-      @@rwaccessors.each do |e|
-        next if e.to_s =~ /\A(?:addresser|recipient|timestamp)\z/
-        data[e.to_s] = self.send(e)
-      end
-      data['addresser'] = self.addresser.to_s
-      data['recipient'] = self.recipient.to_s
-      data['timestamp'] = self.timestamp.to_time.to_i
-      return data
+    # JSON handler
+    # @return   [String]            JSON string converted from Sisimai::Data
+    def to_json(*)
+      return self.dump('json')
     end
 
   end
