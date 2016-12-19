c900b98736463023774f06c846657a72aafb9eaa
azumakuniyuki <azumakuniyuki@gmail.com>
2016-12-19 17:11:59 +0900

    Import changes from https://github.com/sisimai/p5-Sisimai/commit/111f081914e636819144dd7030f2a7909d7309eb

--- lib/sisimai/mta.rb
+++ lib/sisimai/mta.rb
@@ -3,40 +3,12 @@ module Sisimai
   module MTA
     class << self
       # Imported from p5-Sisimail/lib/Sisimai/MTA.pm
+      require 'sisimai/skeleton'
       require 'sisimai/rfc5322'
 
-      # Flags for position variable for
-      # @return   [Hash] Position flag data
-      # @private
-      def INDICATORS
-        return {
-          :'deliverystatus' => (1 << 1),
-          :'message-rfc822' => (1 << 2),
-        }
-      end
-
-      # Data structure for parsed bounce messages
-      # @return [Hash] Data structure for delivery status
-      # @private
-      def DELIVERYSTATUS
-        return {
-          'spec'         => nil,  # Protocl specification
-          'date'         => nil,  # The value of Last-Attempt-Date header
-          'rhost'        => nil,  # The value of Remote-MTA header
-          'lhost'        => nil,  # The value of Received-From-MTA header
-          'alias'        => nil,  # The value of alias entry(RHS)
-          'agent'        => nil,  # MTA name
-          'action'       => nil,  # The value of Action header
-          'status'       => nil,  # The value of Status header
-          'reason'       => nil,  # Temporary reason of bounce
-          'command'      => nil,  # SMTP command in the message body
-          'replycode'    => nil,  # SMTP Reply code
-          'diagnosis'    => nil,  # The value of Diagnostic-Code header
-          'recipient'    => nil,  # The value of Final-Recipient header
-          'softbounce'   => nil,  # Soft bounce or not
-          'feedbacktype' => nil,  # Feedback Type
-        }
-      end
+      def INDICATORS;        return Sisimai::Skeleton.INDICATORS;     end
+      def DELIVERYSTATUS;    return Sisimai::Skeleton.DELIVERYSTATUS; end
+      def smtpagent(v = ''); return v.to_s.sub(/\ASisimai::/, '');    end
 
       # MTA list
       # @return   [Array] MTA list with order
