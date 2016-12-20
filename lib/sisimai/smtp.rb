f20ec3fb5b11da4bafe1a408c55e2d3194548fd9
azumakuniyuki <azumakuniyuki@gmail.com>
2016-12-20 20:21:27 +0900

    Removed Sisimai::SMTP.is_softbounce() method: https://github.com/sisimai/p5-Sisimai/commit/0d8990aaef0f218434464dc0be664eb53ae4b653

--- lib/sisimai/smtp.rb
+++ lib/sisimai/smtp.rb
@@ -4,55 +4,18 @@ module Sisimai
     class << self
       # Imported from p5-Sisimail/lib/Sisimai/SMTP.pm
 
-      # Check softbounce or not
-      # @param    [String] argv1    String including SMTP Status code
-      # @return   [True,False,Nil]  true:  Soft bounce
-      #                             false: Hard bounce
-      #                             nil: May not be bounce ?
-      def is_softbounce(argv1 = '')
-        warn sprintf(" ***warning: Obsoleted method, use Sisimai::SMTP::Error.soft_or_hard() instead.");
-        return nil unless argv1
-        return nil unless argv1.size > 0
-
-        mesgstring = argv1.to_s
-        classvalue = nil
-        softbounce = -1
-
-        if cv = mesgstring.match(/\b([245])\d\d\b/)
-          # Valid SMTP reply code such as 550, 421
-          classvalue = cv[1].to_i
-
-        elsif cv = mesgstring.match(/\b([245])[.][0-9][.]\d+\b/)
-          # DSN value such as 5.1.1, 4.4.7
-          classvalue = cv[1].to_i
-        end
-
-        if classvalue == 4
-          # Soft bounce, Persistent transient error
-          softbounce = true
-
-        elsif classvalue == 5
-          # Hard bounce, Permanent error
-          softbounce = false
-        else
-          # Check with regular expression
-          if mesgstring =~ /(?:temporar|persistent)/i
-            # Temporary failure
-            softbounce = true
-
-          elsif mesgstring =~ /permanent/i
-            # Permanently failure
-            softbounce = false
-
-          else
-            # did not find information to decide that it is a soft bounce
-            # or a hard bounce.
-            softbounce = nil
-          end
-        end
-
-        return softbounce
+      # Detector for SMTP commands in a bounce mail message
+      # @private
+      # @return   [Hash] SMTP command regular expressions
+      def command
+        return {
+            :helo => %r/\b(?:HELO|EHLO)\b/,
+            :mail => %r/\bMAIL F(?:ROM|rom)\b/,
+            :rcpt => %r/\bRCPT T[Oo]\b/,
+            :data => %r/\bDATA\b/,
+        }
       end
+
     end
   end
 end
