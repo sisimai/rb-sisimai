991504249f7cc90b0f2a00517a8ebe9412717fc4
azumakuniyuki <azumakuniyuki@gmail.com>
2016-12-21 20:23:25 +0900

    Follow updates on https://github.com/sisimai/p5-Sisimai/commit/0d8990aaef0f218434464dc0be664eb53ae4b653

--- spec/sisimai/smtp_spec.rb
+++ spec/sisimai/smtp_spec.rb
@@ -3,35 +3,24 @@ require 'sisimai/smtp'
 
 describe Sisimai::SMTP do
   cn = Sisimai::SMTP
-  let(:rv) { cn.is_softbounce(se) }
+  rv = cn.command
 
-  describe '.is_softbounce' do
-    context 'reply code is 4XX' do
-      let(:se) { '450 4.7.1 Client host rejected' }
-      subject { rv }
-      it('returns True') { is_expected.to be true }
-      it('returns True') { expect(cn.is_softbounce(421)).to be true }
-    end
-
-    context 'reply code is 5XX' do
-      let(:se) { '553 5.3.5 system config error' }
-      subject { rv }
-      it('returns False') { is_expected.to be false }
-    end
-
-    context 'reply code is neither 4xx nor 5xx' do
-      let(:se) { '250 OK' }
-      subject { rv }
-      it('returns nil') { is_expected.to be nil }
-      it('returns nil') { expect(cn.is_softbounce).to be nil }
+  describe '.command' do
+    context 'No arguments' do
+      it('returns Hash') { expect(rv).to be_a Hash }
+      %w|helo mail rcpt data|.each do |e|
+        it('Key "' + e + '" exists') { expect(rv.key?(e.to_sym)).to be true }
+        it(e + 'is Regexp object')   { expect(rv[e.to_sym]).to be_a Regexp }
+      end
     end
 
     context 'wrong number of arguments' do
       it 'raises ArgumentError' do
-        expect { cn.is_softbounce(nil, nil) }.to raise_error(ArgumentError)
+        expect { cn.command(nil) }.to raise_error(ArgumentError)
+        expect { cn.command(nil, nil) }.to raise_error(ArgumentError)
       end
     end
-
   end
+
 end
 
