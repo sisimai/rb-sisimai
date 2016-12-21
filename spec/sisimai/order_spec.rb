6b6f9cb6bb39298858c1c31573aa499e1e8a4ebc
azumakuniyuki <azumakuniyuki@gmail.com>
2016-12-21 21:20:39 +0900

    Import changes from https://github.com/sisimai/p5-Sisimai/commit/b730600cff2367ca19888384a13ecccb7ff6ff56

--- spec/sisimai/order_spec.rb
+++ spec/sisimai/order_spec.rb
@@ -4,51 +4,18 @@ require 'sisimai/order'
 describe Sisimai::Order do
   cn = Sisimai::Order
   describe '.default' do
-    default = cn.default
-    subject { default }
-    it('returns Array') { is_expected.to be_a Array }
-    it 'have values' do
-      # pending 'Sisimai::MTA::* and Sisimai::MSP::* are not implemented yet'
-      expect(default.size).to be > 0
-    end
+    it('returns Array') { expect(cn.default).to be_a Array }
   end
   describe '.another' do
-    another = cn.another
-    subject { another }
-    it('returns Array') { is_expected.to be_a Array }
-    it('have values')   { expect(another.size).to be > 0 }
-    another.each do |e|
-      it('is a module') { expect(e.class).to be_a Class }
-      it('has a module name') { expect(e.to_s).to match(/\ASisimai::M(?:TA|SP)::/) }
-    end
+    it('returns Array') { expect(cn.another).to be_a Array }
   end
+
   describe '.headers' do
-    headers = cn.headers
-    subject { headers }
-    it('returns Hash') { is_expected.to be_a Hash }
-    headers.each_key do |e|
-      it('has a Hash') { expect(headers[e]).to be_a Hash }
-      it 'have some header names' do
-        expect(headers[e].keys.size).to be > 0
-      end
-      headers[e].each_key do |f|
-        it('has a value(1)') { expect(headers[e][f]).to be == 1 }
-      end
-    end
+    it('returns Hash') { expect(cn.headers).to be_a Hash }
   end
 
   describe '.by("subject")' do
-    orderby = cn.by('subject')
-    subject { orderby }
-    it('returns Hash') { is_expected.to be_a Hash }
-    orderby.each_key do |e|
-      it('is a regular expression') { expect(e).to be_a Regexp }
-      it('is Array') { expect(orderby[e]).to be_a Array }
-      orderby[e].each do |f|
-        it('is String') { expect(f).to be_a String }
-        it('is a module name') { expect(f).to match(/\ASisimai::M(?:TA|SP)::/) }
-      end
-    end
+    it('returns Hash') { expect(cn.by).to be_a Hash }
   end
 end
 
