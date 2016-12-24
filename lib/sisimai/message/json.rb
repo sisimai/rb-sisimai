e14d2d31758ef8f1f252a93685fb8ad03432a97d
azumakuniyuki <azumakuniyuki@gmail.com>
2016-12-24 14:53:39 +0900

    Fix bug for make test

---
+++ lib/sisimai/message/json.rb
@@ -0,0 +1,196 @@
+module Sisimai
+  # Sisimai::Message::JSON convert from a bounce object (decoded JSON) which is
+  # retrieved from some Cloud Email Deliveries API to data structure.
+  class Message
+    class JSON
+      # Imported from p5-Sisimail/lib/Sisimai/Message/JSON.pm
+      require 'sisimai/order/json'
+
+      @@ToBeLoaded = []
+      @@TryOnFirst = []
+
+      DefaultSet = Sisimai::Order::JSON.default
+      ObjectKeys = Sisimai::Order::JSON.by('keyname')
+
+      # Make data structure from decoded JSON object
+      # @param         [Hash] argvs   Bounce object
+      # @options argvs [Hash]   data  Decoded JSON
+      # @options argvs [Array]  load  User defined CED module list
+      # @options argvs [Array]  order The order of CED modules
+      # @options argvs [Code]   hook  Reference to callback method
+      # @return        [Hash]         Resolved data structure
+      def self.make(argvs)
+        hookmethod = argvs['hook'] || nil
+        processing = {
+          'from'   => '',  # From_ line
+          'header' => {},  # Email header
+          'rfc822' => '',  # Original message part
+          'ds'     => [],  # Parsed data, Delivery Status
+          'catch'  => nil, # Data parsed by callback method
+        }
+        methodargv = {
+          'load'  => argvs['load'] || [],
+          'order' => argvs['order'] || []
+        }
+        @@ToBeLoaded = Sisimai::Message::JSON.load(methodargv)
+        @@TryOnFirst = Sisimai::Message::JSON.makeorder(argvs['data'])
+
+        # Rewrite message body for detecting the bounce reason
+        methodargv = { 'hook' => hookmethod, 'json' => argvs['data'] }
+        bouncedata = Sisimai::Message::JSON.parse(methodargv)
+
+        return nil unless bouncedata
+        return nil if bouncedata.empty?
+        processing['ds']     = bouncedata['ds']
+        processing['catch']  = bouncedata['catch']
+        processing['rfc822'] = bouncedata['rfc822']
+
+        return processing
+      end
+
+      # Load CED  modules which specified at 'order' and 'load' in the argument
+      # @param         [Hash] argvs       Module information to be loaded
+      # @options argvs [Array]  load      User defined CED module list
+      # @options argvs [Array]  order     The order of CED modules
+      # @return        [Array]            Module list
+      # @since v4.20.0
+      def self.load(argvs)
+        modulelist = []
+        tobeloaded = []
+
+        %w|load order|.each do |e|
+          # The order of CED modules specified by user
+          next unless argvs.key?(e)
+          next unless argvs[e].is_a? Array
+          next if argvs[e].empty?
+
+          modulelist.concat(argvs['order']) if e == 'order'
+          next unless e == 'load'
+
+          # Load user defined CED module
+          argvs['load'].each do |v|
+            # Load user defined CED module
+            begin
+              require v.to_s.gsub('::', '/').downcase
+            rescue LoadError
+              warn ' ***warning: Failed to load ' + v
+              next
+            end
+            tobeloaded << v
+          end
+        end
+
+        modulelist.each do |e|
+          # Append the custom order of CED modules
+          next if tobeloaded.index(e)
+          tobeloaded << e
+        end
+
+        return tobeloaded
+      end
+
+      # Check the decoded JSON strucutre for detecting CED modules and returns
+      # the order of modules to be called.
+      # @param         [Hash] heads   Decoded JSON object
+      # @return        [Array]        Order of CED modules
+      def self.makeorder(argvs)
+        return [] unless argvs
+        return [] unless argvs.keys.size > 0
+        order = []
+
+        # Seek some key names from given argument
+        ObjectKeys.each_key do |e|
+          # Get CED module list matched with a specified key
+          next unless argvs.key?(e)
+
+          # Matched and push it into the order list
+          order.concat(ObjectKeys[e])
+          break
+        end
+        return order
+      end
+
+      # Parse bounce object with each CED module
+      # @param               [Hash] argvs    Processing message entity.
+      # @param options argvs [Hash] json     Decoded bounce object
+      # @param options argvs [Proc] hook     Hook method to be called
+      # @return              [Hash]          Parsed and structured bounce mails
+      def self.parse(argvs)
+        bouncedata = argvs['json'] || {}
+        hookmethod = argvs['hook'] || nil
+        havecaught = nil
+        haveloaded = {}
+        hasadapted = nil
+
+        # Call the hook method
+        if hookmethod.is_a? Proc
+          # Execute hook method
+          begin
+            p = { 'headers' => {}, 'message' => argvs['json'] }
+            havecaught = hookmethod.call(p)
+          rescue StandardError => ce
+            warn sprintf(" ***warning: Something is wrong in hook method :%s", ce.to_s)
+          end
+        end
+
+        catch :ADAPTOR do
+          loop do
+            # 1. User-Defined Module
+            # 2. CED Module Candidates to be tried on first
+            # 3. Sisimai::CED::*
+            #
+            @@ToBeLoaded.each do |r|
+              # Call user defined CED modules
+              next if haveloaded[r]
+              begin
+                require r.gsub('::', '/').downcase
+              rescue LoadError => ce
+                warn ' ***warning: Failed to load ' + ce.to_s
+                next
+              end
+              hasadapted = Module.const_get(r).adapt(bouncedata)
+              haveloaded[r] = true
+              throw :ADAPTOR if hasadapted
+            end
+
+            @@TryOnFirst.each do |r|
+              # Try CED module candidates which are detected from object key names
+              next if haveloaded.key?(r)
+              begin
+                require r.gsub('::', '/').downcase
+              rescue LoadError => ce
+                warn ' ***warning: ' + ce.to_s
+                next
+              end
+              hasadapted = Module.const_get(r).adapt(bouncedata)
+              haveloaded[r] = true
+              throw :ADAPTOR if hasadapted
+            end
+
+            DefaultSet.each do |r|
+              # Default order of CED modules
+              next if haveloaded.key?(r)
+              begin
+                require r.gsub('::', '/').downcase
+              rescue => ce
+                warn ' ***warning: ' + ce.to_s
+                next
+              end
+
+              hasadapted = Module.const_get(r).adapt(bouncedata)
+              haveloaded[r] = true
+              throw :ADAPTOR if hasadapted
+            end
+
+            break # as of now, we have no sample JSON data for coding this block
+          end
+        end
+
+        hasadapted['catch'] = havecaught if hasadapted
+        return hasadapted
+      end
+
+    end
+  end
+end
+
