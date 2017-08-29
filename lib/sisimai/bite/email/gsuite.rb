d56fc6a0b1a808095eb2793f331bce70f6abbc55
azumakuniyuki <azumakuniyuki@gmail.com>
2017-08-29 19:36:48 +0900

    Fix code: wrong value assignment...

--- lib/sisimai/bite/email/gsuite.rb
+++ lib/sisimai/bite/email/gsuite.rb
@@ -162,7 +162,7 @@ module Sisimai::Bite::Email
               elsif cv = e.match(/\A[Aa]rrival-[Dd]ate:[ ]*(.+)\z/)
                 # Arrival-Date: Wed, 29 Apr 2009 16:03:18 +0900
                 next if connheader['date'].size > 0
-                connheader['date'] = 1
+                connheader['date'] = cv[1]
                 connvalues += 1
 
               else
