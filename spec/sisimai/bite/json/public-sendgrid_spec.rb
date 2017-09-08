12ce1ba05bd7bf6c0feba3b87983a48d449e275a
azumakuniyuki <azumakuniyuki@gmail.com>
2017-09-09 01:26:19 +0900

    Add new sample JSON object retrieved from SendGrid API(outlook) imported from sisimai/p5-Sisimai@5690448

--- spec/sisimai/bite/json/public-sendgrid_spec.rb
+++ spec/sisimai/bite/json/public-sendgrid_spec.rb
@@ -17,6 +17,7 @@ isexpected = [
   { 'n' => '13', 's' => /\A2[.]0[.]0\z/,   'r' => /delivered/,   'b' => /\A-1\z/},
   { 'n' => '14', 's' => /\A5[.]7[.]13\z/,  'r' => /suspend/,     'b' => /\A1\z/ },
   { 'n' => '15', 's' => /\A\z/,            'r' => /feedback/,    'b' => /\A-1\z/ },
+  { 'n' => '16', 's' => /\A\z/,            'r' => /feedback/,    'b' => /\A-1\z/ },
 ]
 Sisimai::Bite::JSON::Code.maketest(enginename, isexpected)
 
