2e0b85f35a32f197d0f649191533478158adf0dd
azumakuniyuki <azumakuniyuki@gmail.com>
2018-04-17 12:51:34 +0900

    Fix the test result for email-ezweb-02.eml (sisimai/p5-Sisimai@4cbd035b)

--- spec/sisimai/bite/email/public-ezweb_spec.rb
+++ spec/sisimai/bite/email/public-ezweb_spec.rb
@@ -3,7 +3,7 @@ require './spec/sisimai/bite/email/code'
 enginename = 'EZweb'
 isexpected = [
   { 'n' => '01', 's' => /\A5[.]0[.]\d+\z/, 'r' => /filtered/,    'b' => /\A1\z/ },
-  { 'n' => '02', 's' => /\A5[.]0[.]\d+\z/, 'r' => /(?:suspend|undefined)/, 'b' => /\d\z/ },
+  { 'n' => '02', 's' => /\A5[.]0[.]\d+\z/, 'r' => /suspend/,     'b' => /\A1\z/ },
   { 'n' => '03', 's' => /\A5[.]0[.]\d+\z/, 'r' => /suspend/,     'b' => /\A1\z/ },
   { 'n' => '04', 's' => /\A5[.]0[.]\d+\z/, 'r' => /userunknown/, 'b' => /\A0\z/ },
   { 'n' => '05', 's' => /\A5[.]0[.]\d+\z/, 'r' => /expired/,     'b' => /\A1\z/ },
