05fdf21b1d1cb34151e4a8c3d03411a362d8332e
azumakuniyuki <azumakuniyuki@gmail.com>
2018-12-17 11:22:22 +0900

    Use Sisimai::RFC1894

--- lib/sisimai/bite/email/surfcontrol.rb
+++ lib/sisimai/bite/email/surfcontrol.rb
@@ -37,6 +37,8 @@ module Sisimai::Bite::Email
         return nil unless mhead['x-mailer']
         return nil unless mhead['x-mailer'] == 'SurfControl E-mail Filter'
 
+        require 'sisimai/rfc1894'
+        fieldtable = Sisimai::RFC1894.FIELDTABLE
         dscontents = [Sisimai::Bite.DELIVERYSTATUS]
         hasdivided = mbody.split("\n")
         havepassed = ['']
@@ -111,23 +113,18 @@ module Sisimai::Bite::Email
               v['diagnosis'] = cv[2]
             else
               # Fallback, parse RFC3464 headers.
-              if cv = e.match(/\ADiagnostic-Code:[ ]*(.+?);[ ]*(.+)\z/)
-                # Diagnostic-Code: SMTP; 550 5.1.1 <userunknown@example.jp>... User Unknown
-                v['spec'] = cv[1].upcase
-                v['diagnosis'] = cv[2]
-
-              elsif p.start_with?('Diagnostic-Code:') && cv = e.match(/\A[ ]+(.+)\z/)
-                # Continued line of the value of Diagnostic-Code header
+              if f = Sisimai::RFC1894.match(e)
+                # "e" matched with any field defined in RFC3464
+                next unless o = Sisimai::RFC1894.field(e)
+                next if o[1] == 'final-recipient'
+                next unless fieldtable.key?(o[0].to_sym)
+                v[fieldtable[o[0].to_sym]] = o[2]
+              else
+                # Continued line of the value of Diagnostic-Code field
+                next unless p.start_with?('Diagnostic-Code:')
+                next unless cv = e.match(/\A[ \t]+(.+)\z/)
                 v['diagnosis'] << ' ' << cv[1]
                 havepassed[-1] = 'Diagnostic-Code: ' << e
-
-              elsif cv = e.match(/\AAction:[ ]*(.+)\z/)
-                # Action: failed
-                v['action'] = cv[1].downcase
-
-              elsif cv = e.match(/\AStatus:[ ]*(\d[.]\d+[.]\d+)/)
-                # Status: 5.0.-
-                v['status'] = cv[1]
               end
             end
           end
--- spec/sisimai/bite/email/private-office365_spec.rb
+++ spec/sisimai/bite/email/private-office365_spec.rb
@@ -3,10 +3,10 @@ require './spec/sisimai/bite/email/code'
 enginename = 'Office365'
 isexpected = [
   { 'n' => '01001', 'r' => /filtered/ },
-  { 'n' => '01002', 'r' => /filtered/ },
-  { 'n' => '01003', 'r' => /filtered/ },
-  { 'n' => '01004', 'r' => /filtered/ },
-  { 'n' => '01005', 'r' => /filtered/ },
+  { 'n' => '01002', 'r' => /userunknown/ },
+  { 'n' => '01003', 'r' => /userunknown/ },
+  { 'n' => '01004', 'r' => /userunknown/ },
+  { 'n' => '01005', 'r' => /userunknown/ },
   { 'n' => '01006', 'r' => /networkerror/ },
   { 'n' => '01007', 'r' => /userunknown/ },
   { 'n' => '01008', 'r' => /userunknown/ },
