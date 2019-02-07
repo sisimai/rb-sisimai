cce579f7534e44b7ed5f8cc86f08cec3d476d2eb
azumakuniyuki <azumakuniyuki@gmail.com>
2019-02-07 16:21:37 +0900

    Less Symbol, Less String#to_sym.

--- lib/sisimai/rhost/googleapps.rb
+++ lib/sisimai/rhost/googleapps.rb
@@ -8,29 +8,29 @@ module Sisimai
         # Imported from p5-Sisimail/lib/Sisimai/Rhost/GoogleApps.pm
         StatusList = {
           # https://support.google.com/a/answer/3726730
-          'X.1.1' => [{ reason: 'userunknown', stirng: ['The email account that you tried to reach does not exist.'] }],
-          'X.1.2' => [{ reason: 'hostunknown', stirng: ["We weren't able to find the recipient domain."] }],
+          'X.1.1' => [{ reason: 'userunknown', string: ['The email account that you tried to reach does not exist.'] }],
+          'X.1.2' => [{ reason: 'hostunknown', string: ["We weren't able to find the recipient domain."] }],
           'X.2.1' => [
-            { reason: 'suspend',   stirng: ['The email account that you tried to reach is disabled.'] },
-            { reason: 'undefined', stirng: ['The user you are trying to contact is receiving mail ']  },
+            { reason: 'suspend',   string: ['The email account that you tried to reach is disabled.'] },
+            { reason: 'undefined', string: ['The user you are trying to contact is receiving mail ']  },
           ],
-          'X.2.2' => [{ reason: 'mailboxfull', stirng: ['The email account that you tried to reach is over quota.'] }],
-          'X.2.3' => [{ reason: 'exceedlimit', stirng: ["Your message exceeded Google's message size limits."] }],
+          'X.2.2' => [{ reason: 'mailboxfull', string: ['The email account that you tried to reach is over quota.'] }],
+          'X.2.3' => [{ reason: 'exceedlimit', string: ["Your message exceeded Google's message size limits."] }],
           'X.3.0' => [
-            { reason: 'syntaxerror', stirng: ['Multiple destination domains per transaction is unsupported.'] },
-            { reason: 'undefined',   stirng: ['Mail server temporarily rejected message.'] },
+            { reason: 'syntaxerror', string: ['Multiple destination domains per transaction is unsupported.'] },
+            { reason: 'undefined',   string: ['Mail server temporarily rejected message.'] },
           ],
-          'X.4.2' => [{ reason: 'expired', stirng: ['Timeout - closing connection.'] }],
+          'X.4.2' => [{ reason: 'expired', string: ['Timeout - closing connection.'] }],
           'X.4.5' => [
-            { reason: 'exceedlimit', stirng: ['Daily sending quota exceeded.'] },
-            { reason: 'undefined',   stirng: ['Server busy, try again later.'] },
+            { reason: 'exceedlimit', string: ['Daily sending quota exceeded.'] },
+            { reason: 'undefined',   string: ['Server busy, try again later.'] },
           ],
-          'X.5.0' => [{ reason: 'syntaxerror', stirng: ['SMTP protocol violation'] }],
+          'X.5.0' => [{ reason: 'syntaxerror', string: ['SMTP protocol violation'] }],
           'X.5.1' => [
-            { reason: 'securityerror', stirng: ['Authentication Required.'] },
+            { reason: 'securityerror', string: ['Authentication Required.'] },
             {
               reason: 'syntaxerror',
-              stirng: [
+              string: [
                 'STARTTLS may not be repeated',
                 'Too many unrecognized commands, goodbye.',
                 'Unimplemented command.',
@@ -42,51 +42,51 @@ module Sisimai
             },
           ],
           'X.5.2' => [
-            { reason: 'securityerror', stirng: ['Cannot Decode response.'] },   # 2FA related error, maybe.
-            { reason: 'syntaxerror',   stirng: ['Syntax error.'] },
+            { reason: 'securityerror', string: ['Cannot Decode response.'] },   # 2FA related error, maybe.
+            { reason: 'syntaxerror',   string: ['Syntax error.'] },
           ],
           'X.5.3' => [
-            { reason: 'mailboxfull',    stirng: ['Domain policy size per transaction exceeded,'] },
-            { reason: 'policyviolation',stirng: ['Your message has too many recipients.'] },
+            { reason: 'mailboxfull',    string: ['Domain policy size per transaction exceeded,'] },
+            { reason: 'policyviolation',string: ['Your message has too many recipients.'] },
           ],
-          'X.5.4' => [{ reason: 'syntaxerror', stirng: ['Optional Argument not permitted for that AUTH mode.'] }],
+          'X.5.4' => [{ reason: 'syntaxerror', string: ['Optional Argument not permitted for that AUTH mode.'] }],
           'X.6.0' => [
-            { reason: 'contenterror', stirng: ['Mail message is malformed.'] },
-            { reason: 'networkerror', stirng: ['Message exceeded 50 hops'] }
+            { reason: 'contenterror', string: ['Mail message is malformed.'] },
+            { reason: 'networkerror', string: ['Message exceeded 50 hops'] }
           ],
           'X.7.0' => [
             {
               reason: 'blocked',
-              stirng: [
+              string: [
                 'IP not in whitelist for RCPT domain, closing connection.',
                 'Our system has detected an unusual rate of unsolicited mail originating from your IP address.',
               ],
             },
             {
               reason: 'expired',
-              stirng: [
+              string: [
                 'Temporary System Problem. Try again later.',
                 'Try again later, closing connection.',
               ],
             },
             {
               reason: 'securityerror',
-              stirng: [
+              string: [
                 'TLS required for RCPT domain, closing connection.',
                 'No identity changes permitted.',
                 'Must issue a STARTTLS command first.',
                 'Too Many Unauthenticated commands.',
               ],
             },
-            { reason: 'systemerror', stirng: ['Cannot authenticate due to temporary system problem.'] },
-            { reason: 'norelaying',  stirng: ['Mail relay denied.'] },
-            { reason: 'rejected',    stirng: ['Mail Sending denied.'] },
+            { reason: 'systemerror', string: ['Cannot authenticate due to temporary system problem.'] },
+            { reason: 'norelaying',  string: ['Mail relay denied.'] },
+            { reason: 'rejected',    string: ['Mail Sending denied.'] },
           ],
           'X.7.1' => [
-            { reason: 'mailboxfull', stirng: ['Email quota exceeded.'] },
+            { reason: 'mailboxfull', string: ['Email quota exceeded.'] },
             {
               reason: 'securityerror',
-              stirng: [
+              string: [
                 'Application-specific password required.',
                 'Please log in with your web browser and then try again.',
                 'Username and Password not accepted.',
@@ -94,16 +94,16 @@ module Sisimai
             },
             {
               reason: 'blocked',
-              stirng: [
+              string: [
                 'Our system has detected an unusual rate of unsolicited mail originating from your IP address.',
                 "The IP you're using to send mail is not authorized to send email directly to our servers.",
               ],
             },
-            { reason: 'spamdetected',   stirng: ['Our system has detected that this message is likely unsolicited mail.'] },
-            { reason: 'policyviolation',stirng: ['The user or domain that you are sending to (or from) has a policy'] },
-            { reason: 'rejected',       stirng: ['Unauthenticated email is not accepted from this domain.'] },
+            { reason: 'spamdetected',   string: ['Our system has detected that this message is likely unsolicited mail.'] },
+            { reason: 'policyviolation',string: ['The user or domain that you are sending to (or from) has a policy'] },
+            { reason: 'rejected',       string: ['Unauthenticated email is not accepted from this domain.'] },
           ],
-          'X.7.4' => [{ reason: 'syntaxerror', stirng: ['Unrecognized Authentication Type.'] }],
+          'X.7.4' => [{ reason: 'syntaxerror', string: ['Unrecognized Authentication Type.'] }],
         }.freeze
 
         # Detect bounce reason from Google Apps
@@ -114,12 +114,12 @@ module Sisimai
           return argvs.reason unless argvs.reason.empty?
 
           (statuscode = argvs.deliverystatus.clone)[0] = 'X'
-          return '' unless StatusList[statuscode.to_sym]
+          return '' unless StatusList[statuscode]
 
           reasontext = ''
-          StatusList[statuscode.to_sym].each do |e|
+          StatusList[statuscode].each do |e|
             # Try to match
-            next unless e[:regexp].any? { |a| argvs.diagnosticcode.include?(a) }
+            next unless e[:string].any? { |a| argvs.diagnosticcode.include?(a) }
             reasontext = e[:reason]
             break
           end
