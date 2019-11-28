d84749bb7a259839caa89435c074299a33311893
azumakuniyuki <azumakuniyuki@gmail.com>
2019-11-28 14:12:09 +0900

    Test code for Sisimai::Order (test code for child classes have been merged into spec/sisimai/order_spec.rb

--- spec/sisimai/order_spec.rb
+++ spec/sisimai/order_spec.rb
@@ -3,19 +3,66 @@ require 'sisimai/order'
 
 describe Sisimai::Order do
   cn = Sisimai::Order
-  describe '.default' do
-    it('returns Array') { expect(cn.default).to be_a Array }
-  end
-  describe '.another' do
-    it('returns Array') { expect(cn.another).to be_a Array }
+
+  describe '.make' do
+    pattern = cn.make({ 'subject' => 'delivery failure' })
+    subject { pattern }
+    it('returns Array') { is_expected.to be_a Array }
+    it('have values')   { expect(pattern.size).to be > 0 }
+    pattern.each do |e|
+      it('is a module') { expect(e.class).to be_a Class }
+      it('has a module name') { expect(e.to_s).to match(/\ASisimai::Lhost::/) }
+    end
   end
 
+  describe '.default' do
+    default = cn.default
+    subject { default }
+    it('returns Array') { is_expected.to be_a Array }
+    it('have values')   { expect(default.size).to be > 0 }
+    default.each do |e|
+      it('is a module') { expect(e.class).to be_a Class }
+      it('has a module name') { expect(e.to_s).to match(/\ASisimai::Lhost::/) }
+    end
+  end
+
+  describe '.another' do
+    another = cn.another
+    subject { another }
+    it('returns Array') { is_expected.to be_a Array }
+    it('have values')   { expect(another.size).to be > 0 }
+    another.each do |e|
+      it('is a module') { expect(e.class).to be_a Class }
+      it('has a module name') { expect(e.to_s).to match(/\ASisimai::Lhost::/) }
+    end
+  end
   describe '.headers' do
-    it('returns Hash') { expect(cn.headers).to be_a Hash }
+    headers = cn.headers
+    subject { headers }
+    it('returns Hash') { is_expected.to be_a Hash }
+    headers.each_key do |e|
+      it('has an Array') { expect(headers[e]).to be_a Array }
+      it 'have some module names' do
+        expect(headers[e].size).to be > 0
+      end
+      headers[e].each do |f|
+        it('has a String') { expect(f).to be_a String }
+      end
+    end
   end
 
   describe '.by("subject")' do
-    it('returns Hash') { expect(cn.by).to be_a Hash }
+    orderby = cn.by('subject')
+    subject { orderby }
+    it('returns Hash') { is_expected.to be_a Hash }
+    orderby.each_key do |e|
+      it('is a String') { expect(e).to be_a String }
+      it('is an Array') { expect(orderby[e]).to be_a Array }
+      orderby[e].each do |f|
+        it('is String') { expect(f).to be_a String }
+        it('is a module name') { expect(f).to match(/\ASisimai::Lhost::/) }
+      end
+    end
   end
 end
 
