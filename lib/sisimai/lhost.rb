61e9b9a4bdbf13ae1baf40cddb8cb765164b14e1
azumakuniyuki <azumakuniyuki@gmail.com>
2019-11-28 13:54:59 +0900

    Require Sisiai::RFC5322

--- lib/sisimai/lhost.rb
+++ lib/sisimai/lhost.rb
@@ -3,6 +3,7 @@ module Sisimai
   module Lhost
     class << self
       # Imported from p5-Sisimail/lib/Sisimai/Lhost.pm
+      require 'sisimai/rfc5322'
 
       # Data structure for parsed bounce messages
       # @return [Hash] Data structure for delivery status
