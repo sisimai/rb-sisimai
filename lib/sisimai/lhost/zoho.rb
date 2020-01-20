f5f5dee9b2e8fb1ee6ce77269de86e6485b488ea
azumakuniyuki <azumakuniyuki@gmail.com>
2020-01-20 14:41:16 +0900

    Sisimai::Lhost::Z* Faster #171

--- lib/sisimai/lhost/zoho.rb
+++ lib/sisimai/lhost/zoho.rb
@@ -7,10 +7,8 @@ module Sisimai::Lhost
       require 'sisimai/lhost'
 
       Indicators = Sisimai::Lhost.INDICATORS
-      StartingOf = {
-        message: ['This message was created automatically by mail delivery'],
-        rfc822:  ['from mail.zoho.com by mx.zohomail.com'],
-      }.freeze
+      ReBackbone = %r|^Received:[ ]*from[ ]mail[.]zoho[.]com[ ]by[ ]mx[.]zohomail[.]com|.freeze
+      StartingOf = { message: ['This message was created automatically by mail delivery'] }.freeze
       MessagesOf = { 'expired' => ['Host not reachable'] }.freeze
 
       def description; return 'Zoho Mail: https://www.zoho.com'; end
@@ -43,90 +41,69 @@ module Sisimai::Lhost
         return nil unless mhead['x-zohomail']
 
         dscontents = [Sisimai::Lhost.DELIVERYSTATUS]
-        hasdivided = mbody.split("\n")
-        rfc822list = []     # (Array) Each line in message/rfc822 part string
-        blanklines = 0      # (Integer) The number of blank lines
+        emailsteak = Sisimai::RFC5322.fillet(mbody, ReBackbone)
+        bodyslices = emailsteak[0].split("\n")
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
         qprintable = false
         v = nil
 
-        while e = hasdivided.shift do
+        while e = bodyslices.shift do
+          # Read error messages and delivery status lines from the head of the email
+          # to the previous line of the beginning of the original message.
           if readcursor == 0
             # Beginning of the bounce message or delivery status part
-            if e.start_with?(StartingOf[:message][0])
-              readcursor |= Indicators[:deliverystatus]
-              next
-            end
+            readcursor |= Indicators[:deliverystatus] if e.start_with?(StartingOf[:message][0])
+            next
           end
+          next if (readcursor & Indicators[:deliverystatus]) == 0
+          next if e.empty?
 
-          if (readcursor & Indicators[:'message-rfc822']) == 0
-            # Beginning of the original message part
-            if e.include?(StartingOf[:rfc822][0])
-              readcursor |= Indicators[:'message-rfc822']
-              next
-            end
-          end
+          # This message was created automatically by mail delivery software.
+          # A message that you sent could not be delivered to one or more of its recip=
+          # ients. This is a permanent error.=20
+          #
+          # kijitora@example.co.jp Invalid Address, ERROR_CODE :550, ERROR_CODE :5.1.=
+          # 1 <kijitora@example.co.jp>... User Unknown
 
-          if readcursor & Indicators[:'message-rfc822'] > 0
-            # Inside of the original message part
-            if e.empty?
-              blanklines += 1
-              break if blanklines > 1
-              next
-            end
-            rfc822list << e
-          else
-            # Error message part
-            next if (readcursor & Indicators[:deliverystatus]) == 0
-            next if e.empty?
+          # This message was created automatically by mail delivery software.
+          # A message that you sent could not be delivered to one or more of its recipients. This is a permanent error.
+          #
+          # shironeko@example.org Invalid Address, ERROR_CODE :550, ERROR_CODE :Requested action not taken: mailbox unavailable
+          v = dscontents[-1]
 
-            # This message was created automatically by mail delivery software.
-            # A message that you sent could not be delivered to one or more of its recip=
-            # ients. This is a permanent error.=20
-            #
+          if cv = e.match(/\A([^ ]+[@][^ ]+)[ \t]+(.+)\z/)
             # kijitora@example.co.jp Invalid Address, ERROR_CODE :550, ERROR_CODE :5.1.=
-            # 1 <kijitora@example.co.jp>... User Unknown
-
-            # This message was created automatically by mail delivery software.
-            # A message that you sent could not be delivered to one or more of its recipients. This is a permanent error.
-            #
-            # shironeko@example.org Invalid Address, ERROR_CODE :550, ERROR_CODE :Requested action not taken: mailbox unavailable
-            v = dscontents[-1]
-
-            if cv = e.match(/\A([^ ]+[@][^ ]+)[ \t]+(.+)\z/)
-              # kijitora@example.co.jp Invalid Address, ERROR_CODE :550, ERROR_CODE :5.1.=
-              if v['recipient']
-                # There are multiple recipient addresses in the message body.
-                dscontents << Sisimai::Lhost.DELIVERYSTATUS
-                v = dscontents[-1]
-              end
-              v['recipient'] = cv[1]
-              v['diagnosis'] = cv[2]
-
-              if v['diagnosis'].end_with?('=')
-                # Quoted printable
-                v['diagnosis'] = v['diagnosis'].chomp('=')
-                qprintable = true
-              end
-              recipients += 1
-
-            elsif cv = e.match(/\A\[Status: .+[<]([^ ]+[@][^ ]+)[>],/)
-              # Expired
-              # [Status: Error, Address: <kijitora@6kaku.example.co.jp>, ResponseCode 421, , Host not reachable.]
-              if v['recipient']
-                # There are multiple recipient addresses in the message body.
-                dscontents << Sisimai::Lhost.DELIVERYSTATUS
-                v = dscontents[-1]
-              end
-              v['recipient'] = cv[1]
-              v['diagnosis'] = e
-              recipients += 1
-            else
-              # Continued line
-              next unless qprintable
-              v['diagnosis'] << e
+            if v['recipient']
+              # There are multiple recipient addresses in the message body.
+              dscontents << Sisimai::Lhost.DELIVERYSTATUS
+              v = dscontents[-1]
             end
+            v['recipient'] = cv[1]
+            v['diagnosis'] = cv[2]
+
+            if v['diagnosis'].end_with?('=')
+              # Quoted printable
+              v['diagnosis'] = v['diagnosis'].chomp('=')
+              qprintable = true
+            end
+            recipients += 1
+
+          elsif cv = e.match(/\A\[Status: .+[<]([^ ]+[@][^ ]+)[>],/)
+            # Expired
+            # [Status: Error, Address: <kijitora@6kaku.example.co.jp>, ResponseCode 421, , Host not reachable.]
+            if v['recipient']
+              # There are multiple recipient addresses in the message body.
+              dscontents << Sisimai::Lhost.DELIVERYSTATUS
+              v = dscontents[-1]
+            end
+            v['recipient'] = cv[1]
+            v['diagnosis'] = e
+            recipients += 1
+          else
+            # Continued line
+            next unless qprintable
+            v['diagnosis'] << e
           end
         end
         return nil unless recipients > 0
@@ -143,8 +120,7 @@ module Sisimai::Lhost
           end
         end
 
-        rfc822part = Sisimai::RFC5322.weedout(rfc822list)
-        return { 'ds' => dscontents, 'rfc822' => rfc822part }
+        return { 'ds' => dscontents, 'rfc822' => emailsteak[1] }
       end
 
     end
