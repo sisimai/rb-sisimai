a21a0a1ee196e001066b39fd79a199ce4d259e82
azumakuniyuki <azumakuniyuki@gmail.com>
2020-02-11 13:34:32 +0900

    Remove unused method: Sisimai::SMTP.command

--- lib/sisimai/smtp.rb
+++ lib/sisimai/smtp.rb
@@ -3,19 +3,6 @@ module Sisimai
   module SMTP
     class << self
       # Imported from p5-Sisimail/lib/Sisimai/SMTP.pm
-
-      # Detector for SMTP commands in a bounce mail message
-      # @private
-      # @return   [Hash] SMTP command regular expressions
-      def command
-        return {
-          helo: %r/\b(?:HELO|EHLO)\b/,
-          mail: %r/\bMAIL F(?:ROM|rom)\b/,
-          rcpt: %r/\bRCPT T[Oo]\b/,
-          data: %r/\bDATA\b/,
-        }
-      end
-
     end
   end
 end
--- spec/sisimai/smtp_spec.rb
+++ spec/sisimai/smtp_spec.rb
@@ -3,24 +3,5 @@ require 'sisimai/smtp'
 
 describe Sisimai::SMTP do
   cn = Sisimai::SMTP
-  rv = cn.command
-
-  describe '.command' do
-    context 'No arguments' do
-      it('returns Hash') { expect(rv).to be_a Hash }
-      %w|helo mail rcpt data|.each do |e|
-        it('Key "' + e + '" exists') { expect(rv.key?(e.to_sym)).to be true }
-        it(e + 'is Regexp object')   { expect(rv[e.to_sym]).to be_a Regexp }
-      end
-    end
-
-    context 'wrong number of arguments' do
-      it 'raises ArgumentError' do
-        expect { cn.command(nil) }.to raise_error(ArgumentError)
-        expect { cn.command(nil, nil) }.to raise_error(ArgumentError)
-      end
-    end
-  end
-
 end
 
