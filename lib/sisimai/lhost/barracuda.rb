b36accc6d8d1109e7a07dd889e1dde4f1f9b064e
azumakuniyuki <azumakuniyuki@gmail.com>
2020-02-26 17:57:29 +0900

    Impelment Sisimai::Lhost::Barracuda #178

---
+++ lib/sisimai/lhost/barracuda.rb
@@ -0,0 +1,104 @@
+module Sisimai::Lhost
+  # Sisimai::Lhost::Barracuda parses a bounce email which created by
+  # amavsid-new. Methods in the module are called from only Sisimai::Message.
+  module Barracuda
+    class << self
+      # Imported from p5-Sisimail/lib/Sisimai/Lhost/Barracuda.pm
+      require 'sisimai/lhost'
+
+      Indicators = Sisimai::Lhost.INDICATORS
+      ReBackbone = %r|^Content-Type:[ ]text/rfc822-headers|.freeze
+      StartingOf = { message: ['Your message to:'] }.freeze
+
+      def description; return 'Barracuda: https://www.barracuda.com'; end
+      def smtpagent;   return Sisimai::Lhost.smtpagent(self); end
+
+      # Parse bounce messages from Barracuda
+      # @param         [Hash] mhead       Message headers of a bounce email
+      # @options mhead [String] from      From header
+      # @options mhead [String] date      Date header
+      # @options mhead [String] subject   Subject header
+      # @options mhead [Array]  received  Received headers
+      # @options mhead [String] others    Other required headers
+      # @param         [String] mbody     Message body of a bounce email
+      # @return        [Hash, Nil]        Bounce data list and message/rfc822
+      #                                   part or nil if it failed to parse or
+      #                                   the arguments are missing
+      def make(mhead, mbody)
+        # Subject: **Message you sent blocked by our bulk email filter**
+        return nil unless mhead['subject'].to_s.end_with?('our bulk email filter**')
+
+        require 'sisimai/rfc1894'
+        fieldtable = Sisimai::RFC1894.FIELDTABLE
+        permessage = {}     # (Hash) Store values of each Per-Message field
+
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]
+        emailsteak = Sisimai::RFC5322.fillet(mbody, ReBackbone)
+        bodyslices = emailsteak[0].split("\n")
+        readcursor = 0      # (Integer) Points the current cursor position
+        recipients = 0      # (Integer) The number of 'Final-Recipient' header
+        v = nil
+
+        while e = bodyslices.shift do
+          # Read error messages and delivery status lines from the head of the email
+          # to the previous line of the beginning of the original message.
+          if readcursor == 0
+            # Beginning of the bounce message or message/delivery-status part
+            readcursor |= Indicators[:deliverystatus] if e.start_with?(StartingOf[:message][0])
+            next
+          end
+          next if (readcursor & Indicators[:deliverystatus]) == 0
+          next if e.empty?
+
+          if f = Sisimai::RFC1894.match(e)
+            # "e" matched with any field defined in RFC3464
+            next unless o = Sisimai::RFC1894.field(e)
+            v = dscontents[-1]
+
+            if o[-1] == 'addr'
+              # Final-Recipient: rfc822; kijitora@example.jp
+              # X-Actual-Recipient: rfc822; kijitora@example.co.jp
+              if o[0] == 'final-recipient'
+                # Final-Recipient: rfc822; kijitora@example.jp
+                if v['recipient']
+                  # There are multiple recipient addresses in the message body.
+                  dscontents << Sisimai::Lhost.DELIVERYSTATUS
+                  v = dscontents[-1]
+                end
+                v['recipient'] = o[2]
+                recipients += 1
+              else
+                # X-Actual-Recipient: rfc822; kijitora@example.co.jp
+                v['alias'] = o[2]
+              end
+            elsif o[-1] == 'code'
+              # Diagnostic-Code: SMTP; 550 5.1.1 <userunknown@example.jp>... User Unknown
+              v['spec'] = o[1]
+              v['diagnosis'] = o[2]
+            else
+              # Other DSN fields defined in RFC3464
+              next unless fieldtable[o[0]]
+              v[fieldtable[o[0]]] = o[2]
+
+              next unless f == 1
+              permessage[fieldtable[o[0]]] = o[2]
+            end
+          end
+        end
+        return nil unless recipients > 0
+
+        dscontents.each do |e|
+          # Set default values if each value is empty.
+          permessage.each_key { |a| e[a] ||= permessage[a] || '' }
+
+          e['diagnosis'] = Sisimai::String.sweep(e['diagnosis'].to_s.tr("\n", ' '))
+          e['agent'] = self.smtpagent
+        end
+
+        return { 'ds' => dscontents, 'rfc822' => emailsteak[1] }
+      end
+
+    end
+  end
+end
+
