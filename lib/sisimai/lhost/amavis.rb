6a14193bae32e4c0e4905c1cd7c30cadb601da4c
azumakuniyuki <azumakuniyuki@gmail.com>
2020-03-06 16:55:18 +0900

    Define several error reasons and patterns in Sisimai::Lhost::Amavis

--- lib/sisimai/lhost/amavis.rb
+++ lib/sisimai/lhost/amavis.rb
@@ -9,6 +9,65 @@ module Sisimai::Lhost
       Indicators = Sisimai::Lhost.INDICATORS
       ReBackbone = %r|^Content-Type:[ ]text/rfc822-headers|.freeze
       StartingOf = { message: ['The message '] }.freeze
+      MessagesOf = {
+        # amavisd-new-2.11.1/amavisd:1840|%smtp_reason_by_ccat = (
+        # amavisd-new-2.11.1/amavisd:1840|  # currently only used for blocked messages only, status 5xx
+        # amavisd-new-2.11.1/amavisd:1840|  # a multiline message will produce a valid multiline SMTP response
+        # amavisd-new-2.11.1/amavisd:1840|  CC_VIRUS,       'id=%n - INFECTED: %V',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BANNED,      'id=%n - BANNED: %F',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_UNCHECKED.',1', 'id=%n - UNCHECKED: encrypted',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_UNCHECKED.',2', 'id=%n - UNCHECKED: over limits',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_UNCHECKED,      'id=%n - UNCHECKED',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_SPAM,        'id=%n - spam',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_SPAMMY.',1', 'id=%n - spammy (tag3)',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_SPAMMY,      'id=%n - spammy',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',1',   'id=%n - BAD HEADER: MIME error',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',2',   'id=%n - BAD HEADER: nonencoded 8-bit character',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',3',   'id=%n - BAD HEADER: contains invalid control character',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',4',   'id=%n - BAD HEADER: line made up entirely of whitespace',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',5',   'id=%n - BAD HEADER: line longer than RFC 5322 limit',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',6',   'id=%n - BAD HEADER: syntax error',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',7',   'id=%n - BAD HEADER: missing required header field',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH.',8',   'id=%n - BAD HEADER: duplicate header field',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_BADH,        'id=%n - BAD HEADER',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_OVERSIZED,   'id=%n - Message size exceeds recipient\'s size limit',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_MTA.',1',    'id=%n - Temporary MTA failure on relaying',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_MTA.',2',    'id=%n - Rejected by next-hop MTA on relaying',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_MTA,         'id=%n - Unable to relay message back to MTA',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_CLEAN,       'id=%n - CLEAN',
+        # amavisd-new-2.11.1/amavisd:1840|  CC_CATCHALL,    'id=%n - OTHER',  # should not happen
+        # ...
+        # amavisd-new-2.11.1/amavisd:15289|my $status = setting_by_given_contents_category(
+        # amavisd-new-2.11.1/amavisd:15289|  $blocking_ccat,
+        # amavisd-new-2.11.1/amavisd:15289|  { CC_VIRUS,       "554 5.7.0",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_BANNED,      "554 5.7.0",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_UNCHECKED,   "554 5.7.0",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_SPAM,        "554 5.7.0",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_SPAMMY,      "554 5.7.0",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_BADH.",2",   "554 5.6.3",  # nonencoded 8-bit character
+        # amavisd-new-2.11.1/amavisd:15289|    CC_BADH,        "554 5.6.0",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_OVERSIZED,   "552 5.3.4",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_MTA,         "550 5.3.5",
+        # amavisd-new-2.11.1/amavisd:15289|    CC_CATCHALL,    "554 5.7.0",
+        # amavisd-new-2.11.1/amavisd:15289|  });
+        # ...
+        # amavisd-new-2.11.1/amavisd:15332|my $response = sprintf("%s %s%s%s", $status,
+        # amavisd-new-2.11.1/amavisd:15333|  ($final_destiny == D_PASS     ? "Ok" :
+        # amavisd-new-2.11.1/amavisd:15334|   $final_destiny == D_DISCARD  ? "Ok, discarded" :
+        # amavisd-new-2.11.1/amavisd:15335|   $final_destiny == D_REJECT   ? "Reject" :
+        # amavisd-new-2.11.1/amavisd:15336|   $final_destiny == D_BOUNCE   ? "Bounce" :
+        # amavisd-new-2.11.1/amavisd:15337|   $final_destiny == D_TEMPFAIL ? "Temporary failure" :
+        # amavisd-new-2.11.1/amavisd:15338|                                  "Not ok ($final_destiny)" ),
+        'spamdetected'  => [' - spam'],
+        'virusdetected' => [' - infected'],
+        'contenterror'  => [' - bad header:'],
+        'exceedlimit'   => [' - message size exceeds recipient'],
+        'systemerror'   => [
+            ' - temporary mta failure on relaying',
+            ' - rejected by next-hop mta on relaying',
+            ' - unable to relay message back to mta',
+        ],
+      }.freeze
 
       # Parse bounce messages from amavisd-new
       # @param         [Hash] mhead       Message headers of a bounce email
@@ -89,6 +148,19 @@ module Sisimai::Lhost
           # Set default values if each value is empty.
           permessage.each_key { |a| e[a] ||= permessage[a] || '' }
           e['diagnosis'] = Sisimai::String.sweep(e['diagnosis'].to_s.tr("\n", ' '))
+          q = e['diagnosis'].downcase
+          catch :DETECT_REASON do
+            MessagesOf.each_key do |p|
+              # Try to detect an error reason
+              MessagesOf[p].each do |r|
+                # Try to find an error message including lower-cased string
+                # defined in MessagesOf constant
+                next unless q.include?(r)
+                e['reason'] = p
+                throw :DETECT_REASON
+              end
+            end
+          end
         end
         return { 'ds' => dscontents, 'rfc822' => emailsteak[1] }
       end
