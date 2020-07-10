fcfa6b807299dc22e1ebf7c824384556560d262b
azumakuniyuki <azumakuniyuki@gmail.com>
2020-07-10 14:34:17 +0900

    Fix each description of error codes at Sisimai::Rhost::Spectrum

--- lib/sisimai/rhost/spectrum.rb
+++ lib/sisimai/rhost/spectrum.rb
@@ -19,20 +19,20 @@ module Sisimai
           #
           1000 => 'blocked',         # Your IP address has been blocked due to suspicious activity. 
           1010 => 'rejected',        # This email account has been blocked from sending emails due to suspicious activity.
-          1090 => 'systemerror',     # The email you’re trying to send can’t be processed. Try sending again at a later time.
-          1260 => 'networkerror',    # Spectrum doesn’t process IPV6 addresses. Connect with an IPv4 address and try again.
+          1090 => 'systemerror',     # The email you're trying to send can't be processed. Try sending again at a later time.
+          1260 => 'networkerror',    # Spectrum doesn't process IPV6 addresses. Connect with an IPv4 address and try again.
           1500 => 'rejected',        # Your email was rejected for attempting to send as a different email address than you signed in under.
           1520 => 'rejected',        # Your email was rejected for attempting to send as a different email address than a domain that we host.
-          1530 => 'mesgtoobig',      # Your email was rejected because it’s larger than the maximum size of 20MB.
+          1530 => 'mesgtoobig',      # Your email was rejected because it's larger than the maximum size of 20MB.
           1540 => 'toomanyconn',     # Your emails were deferred for attempting to send too many in a single session.
           1550 => 'toomanyconn',     # Your email was rejected for having too many recipients in one message.
           1560 => 'policyviolation', # Your email was rejected for having too many invalid recipients.
         }.freeze
         CodeRanges = [
           [1020, 1080, 'rejected'],        # This email account has limited access to send emails based on suspicious activity.
-          [1100, 1150, 'blocked'],         # The IP address you’re trying to connect from has an issue with the Domain Name System.
-          [1160, 1190, 'policyviolation'], # The email you tried to send goes against your domain’s security policies.
-          [1200, 1210, 'blocked'],         # The IP address you’re trying to send from has been flagged by Cloudmark CSI as potential spam.
+          [1100, 1150, 'blocked'],         # The IP address you're trying to connect from has an issue with the Domain Name System.
+          [1160, 1190, 'policyviolation'], # The email you tried to send goes against your domain's security policies.
+          [1200, 1210, 'blocked'],         # The IP address you're trying to send from has been flagged by Cloudmark CSI as potential spam.
           [1220, 1250, 'blokced'],         # Your IP address has been blacklisted by Spamhaus.
           [1300, 1340, 'toomanyconn'],     # Spectrum limits the number of concurrent connections from a sender
           [1350, 1490, 'toomanyconn'],     # Spectrum limits emails by the number of messages sent, amount of recipients,...
