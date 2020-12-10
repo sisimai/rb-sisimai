99b9d7428c61e10bcd64f1c4409103ee91532866
azumakuniyuki <azumakuniyuki@gmail.com>
2020-12-10 13:58:33 +0900

    Fix super class name < ::DateTime

--- lib/sisimai/time.rb
+++ lib/sisimai/time.rb
@@ -1,8 +1,7 @@
 require 'date'
-
 module Sisimai
   # Sisimai::Time is a child class of Date for Sisimai::Fact
-  class Time < DateTime
+  class Time < ::DateTime
     def to_json(*)
       return self.to_time.to_i
     end
