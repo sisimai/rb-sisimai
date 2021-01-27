15c90fd4da212700dbf53864377530efd8016441
azumakuniyuki <azumakuniyuki@gmail.com>
2021-01-27 22:24:20 +0900

    Test code for Sisimai::Lhost::[XYZ]* using minitest

---
+++ test/public/lhost-x5.rb
@@ -0,0 +1,9 @@
+module LhostEngineTest::Public
+  module X5
+    IsExpected = {
+      # INDEX => [['D.S.N.', 'replycode', 'REASON', 'hardbounce'], [...]]
+      '01' => [['5.1.1',   '550', 'userunknown',     true]],
+    }
+  end
+end
+
