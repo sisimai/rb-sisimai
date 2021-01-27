7d4c392cf5575f277fb9c5b8761a742695e1d308
azumakuniyuki <azumakuniyuki@gmail.com>
2021-01-28 05:01:10 +0900

    Add test code for Sisimai::RFC* and Sisimai::ARF as a Lhost

--- test/public/lhost-engine-test.rb
+++ test/public/lhost-engine-test.rb
@@ -8,15 +8,18 @@ module LhostEngineTest
 
     def test_lhostengine
       directory1 = './test/public'
+      patternset = ['lhost-*.rb', 'arf-*.rb', 'rfc3464.rb', 'rfc3834.rb'].map { |e| e = directory1 + '/' << e }
+      patternset.map { |e| e = directory1 + e }
+
       checksonly = ARGV[0] || ''
       emailindex = ARGV[1] || 0
-      lhostindex = Sisimai::Lhost.index
+      lhostindex = Sisimai::Lhost.index; lhostindex << 'ARF' << 'RFC3464' << 'RFC3834'
+      otherlhost = %w[arf rfc3464 rfc3834]
       enginelist = []
       enginename = ''
-
       checksonly = '' if lhostindex.select { |v| v.downcase == checksonly.downcase }.empty?
 
-      Dir.glob(directory1 + '/lhost-*.rb').each do |f|
+      Dir.glob(patternset).each do |f|
         next if f.end_with?('-test.rb')
         next if f.end_with?('-code.rb')
 
@@ -24,12 +27,21 @@ module LhostEngineTest
           enginelist << checksonly
           break
         else
-          enginelist << f.split('-')[1].sub(/[.]rb\z/, '')
+          if f.include?('-')
+            enginelist << f.split('-')[1].sub(/[.]rb\z/, '')
+          else
+            enginelist << f.split('/')[-1].sub(/[.]rb\z/, '')
+          end
         end
       end
 
       enginelist.each do |e|
-        require sprintf("%s/lhost-%s.rb", directory1, e)
+        if otherlhost.include?(e)
+          require sprintf("%s/%s.rb", directory1, e)
+        else
+          require sprintf("%s/lhost-%s.rb", directory1, e)
+        end
+
         enginename = lhostindex.select { |v| v.downcase == e }.shift
         lhostclass = Module.const_get('LhostEngineTest::Public::' << enginename)
         Lo.enginetest(enginename, lhostclass::IsExpected, false, emailindex)
