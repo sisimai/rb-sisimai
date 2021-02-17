ee46a5f5175189ddcc761af3f6773ec70d0de4a9
azumakuniyuki <azumakuniyuki@gmail.com>
2021-02-17 14:45:49 +0900

    Import commit 99b9d7428c61e10bcd64f1c4409103ee91532866: Fix the super class name

--- lib/sisimai/time.rb
+++ lib/sisimai/time.rb
@@ -1,8 +1,7 @@
 require 'date'
-
 module Sisimai
   # Sisimai::Time is a child class of Date for Sisimai::Data.
-  class Time < DateTime
+  class Time < ::DateTime
     # Imported from p5-Sisimail/lib/Sisimai/Time.pm
     def to_json(*)
       return self.to_time.to_i
