0c7b35275ce3aa57edfd5b1bbf3886dc54af24f8
azumakuniyuki <azumakuniyuki@gmail.com>
2023-02-19 06:22:37 +0900

    Update test to avoid "DEPRECATED: Use assert_nil if expecting nil" message

--- test/public/string-test.rb
+++ test/public/string-test.rb
@@ -73,11 +73,11 @@ class StringTest < Minitest::Test
       ['mx.example.jp (192.0.2.2) reason: 550 5.2.0 Mail rejete.', '192.0.2.2'],
       ['Client host [192.0.2.49] blocked using cbl.abuseat.org (state 13).', '192.0.2.49'],
       ['127.0.0.1', '127.0.0.1'],
-      ['365.31.7.1', nil],
-      ['a.b.c.d', nil],
+      ['365.31.7.1', ''],
+      ['a.b.c.d', ''],
     ]
     ip4address.each do |e|
-      assert_equal e[1], Sisimai::String.ipv4(e[0]).shift
+      assert_equal e[1], Sisimai::String.ipv4(e[0]).shift.to_s
     end
   end
 
