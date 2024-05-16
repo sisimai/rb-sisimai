91d07d20c9ab9b3954c3d390532d87029ea0eabb
azumakuniyuki <azumakuniyuki@gmail.com>
2024-05-16 20:53:58 +0900

    Register rhost-microsoft-04.eml (authfailure)

--- test/public/rhost-microsoft.rb
+++ test/public/rhost-microsoft.rb
@@ -5,6 +5,7 @@ module RhostEngineTest::Public
       '01' => [['5.7.606', '550', 'blocked',         false]],
       '02' => [['5.4.1',   '550', 'userunknown',     true]],
       '03' => [['5.1.10',  '550', 'userunknown',     true]],
+      '04' => [['5.7.509', '550', 'authfailure',     false]],
     }
   end
 end
