850b63285fd5a003cabae7fbba31634a70f9a44d
azumakuniyuki <azumakuniyuki@gmail.com>
2024-06-15 16:35:22 +0900

    Test the results of Sisimai::Lhost::DragonFly #292

---
+++ test/private/lhost-dragonfly.rb
@@ -0,0 +1,9 @@
+module LhostEngineTest::Private
+  module DragonFly
+    IsExpected = {
+      # INDEX => [['D.S.N.', 'replycode', 'REASON', 'hardbounce'], [...]]
+      '01001' => [['5.7.26',  '550', 'authfailure',     false]],
+    }
+  end
+end
+
