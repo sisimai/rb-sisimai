76fb45556219f9256c49c1ce1598f7a9c8dc3ec7
azumakuniyuki <azumakuniyuki@gmail.com>
2024-06-21 17:25:18 +0900

    Add 8 sample emails generated by OpenSMTPD 6.8.0p2

--- test/public/lhost-opensmtpd.rb
+++ test/public/lhost-opensmtpd.rb
@@ -9,6 +9,15 @@ module LhostEngineTest::Public
       '04' => [['5.0.944', '',    'networkerror',    false]],
       '05' => [['5.0.947', '',    'expired',         false]],
       '06' => [['5.0.947', '',    'expired',         false]],
+      '10' => [['5.0.912', '',    'hostunknown',     true]],
+      '11' => [['5.7.26',  '550', 'authfailure',     false]],
+      '12' => [['5.0.932', '',    'notaccept',       true]],
+      '13' => [['4.7.0',   '421', 'blocked',         false]],
+      '14' => [['5.7.25',  '550', 'requireptr',      false]],
+      '15' => [['5.0.947', '',    'expired',         false]],
+      '16' => [['5.0.947', '',    'expired',         false]],
+      '17' => [['5.1.1',   '550', 'userunknown',     true],
+               ['5.2.2',   '552', 'mailboxfull',     false]],
     }
   end
 end
