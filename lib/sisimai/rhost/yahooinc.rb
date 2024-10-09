a0f300d4e508317532285d89abd19e8334a55691
azumakuniyuki <azumakuniyuki@gmail.com>
2024-10-09 16:01:41 +0900

    Fix typo in Sisimai::Rhost::YahooInc #310

--- lib/sisimai/rhost/yahooinc.rb
+++ lib/sisimai/rhost/yahooinc.rb
@@ -23,7 +23,7 @@ module Sisimai
             # - 550 relaying denied for <***@yahoo.com>
             'relaying denied for ',
           ],
-          'notcomplaintrfc' => ['headers are not rfc compliant'],
+          'notcompliantrfc' => ['headers are not rfc compliant'],
           'policyviolation' => [
             # - 554 Message not allowed - [PH01] Email not accepted for policy reasons.
             #   Please visit https://postmaster.yahooinc.com/error-codes
