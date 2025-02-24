426dbb03bbdae45e298d305732e8c0cf1f9546eb
azumakuniyuki <azumakuniyuki@gmail.com>
2025-02-25 08:15:36 +0900

    Bug fix in the error message pattern list

--- lib/sisimai/rhost/microsoft.rb
+++ lib/sisimai/rhost/microsoft.rb
@@ -649,7 +649,7 @@ module Sisimai
             #   connections. (S3115) [Name=Protocol Filter Agent][AGT=PFA][MxId=11BA9B3FA168ABBF]
             #   [BN3PEPF0000B370.namprd21.prod.outlook.com 2025-02-20T14:30:32.425Z 08DD4D9FD5AFF45C]
             #   (in reply to MAIL FROM command))
-            ["4.7.652", "", "", "has exceeded the maximum number of connections"],
+            ["4.7.652", 0, 0, "has exceeded the maximum number of connections"],
 
             # Previous versions of Exchange Server ------------------------------------------------
             ['5.2.122', 0, 0, 'the recipient has exceeded their limit for'],
