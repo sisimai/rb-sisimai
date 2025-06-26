c2740aa0eb2ea383d00ac271973ef355909e574b
azumakuniyuki <azumakuniyuki@gmail.com>
2025-06-27 05:30:57 +0900

    Use "+" operator instead of "<<" in Sisimai::DateTime.parse #347

--- lib/sisimai/datetime.rb
+++ lib/sisimai/datetime.rb
@@ -167,9 +167,7 @@ module Sisimai
         return "" unless argv1.is_a?(::String)
         return "" if argv1.empty?
 
-        datestring = argv1
-        datestring.sub!(/[,](\d+)/, ', \1') # Thu,13 -> Thu, 13
-        datestring.sub!(/(\d{1,2}),/, '\1') # Apr,29 -> Apr 29
+        datestring = argv1.sub(/[,](\d+)/, ', \1').sub(/(\d{1,2}),/, '\1')
         timetokens = datestring.split(' ')
         afternoon1 = 0    # (Integer) After noon flag
         altervalue = {}   # (Hash) To store alternative values
