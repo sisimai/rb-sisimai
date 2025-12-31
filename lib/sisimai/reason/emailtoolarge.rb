d106c368324a962747c708ab203c985c812464fe
azumakuniyuki <azumakuniyuki@gmail.com>
2025-12-31 12:06:49 +0900

    #399 Consolidate "ExceedLimit" and "MesgTooBig" into "EmailTooLarge", "Speeding" and "TooManyConn" into "RateLimited"

--- lib/sisimai/reason/emailtoolarge.rb
+++ lib/sisimai/reason/emailtoolarge.rb
@@ -2,9 +2,9 @@ module Sisimai
   module Reason
     # This is the error that a sent email size is too big for a destination mail server. In many
     # case, There are many attachment files with email, or the file size is too large. Sisimai will
-    # set "mesgtoobig" to the reason of email bounce if the value of Status: field in a bounce email
-    # is "5.3.4".
-    module MesgTooBig
+    # set "emailtoolarge" to the reason of email bounce if the value of Status: field in a bounce
+    # email is "5.2.3" or "5.3.4".
+    module EmailTooLarge
       class << self
         Index = [
           'exceeded maximum inbound message size',
@@ -12,17 +12,18 @@ module Sisimai
           'line limit exceeded',
           'max message size exceeded',
           'message file too big',
+          'message header size exceeds limit',
           'message length exceeds administrative limit',
           'message size exceeds fixed limit',
           'message size exceeds fixed maximum message size',
           'message size exceeds maximum value',
           'message too big',
-          'message too large for this ',
+          'message too large',
           'size limit',
           'taille limite du message atteinte',
         ].freeze
 
-        def text; return 'mesgtoobig'; end
+        def text; return 'emailtoolarge'; end
         def description; return 'Email rejected due to an email size is too big for a destination mail server'; end
 
         # Try to match that the given text and regular expressions
@@ -40,18 +41,18 @@ module Sisimai
         #                                   false: is not big
         # @see http://www.ietf.org/rfc/rfc2822.txt
         def true(argvs)
-          return true if argvs['reason'] == 'mesgtoobig'
+          return true if argvs['reason'] == 'emailtoolarge'
 
           statuscode = argvs['deliverystatus'] || ''
           tempreason = Sisimai::SMTP::Status.name(statuscode)
 
-          # Delivery status code points "mesgtoobig".
+          # Delivery status code points "emailtoolarge".
           # Status: 5.3.4
           # Diagnostic-Code: SMTP; 552 5.3.4 Error: message file too big
-          return true if tempreason == 'mesgtoobig'
-
-          #  5.2.3   Message length exceeds administrative limit
-          return false if( tempreason == 'exceedlimit' || statuscode == '5.2.3' )
+          #
+          # Status: 5.2.3
+          # Deiagnostic-Code: Message length exceeds administrative limit
+          return true if tempreason == 'emailtoolarge'
           return match(argvs['diagnosticcode'].downcase)
         end
 
@@ -59,6 +60,4 @@ module Sisimai
     end
   end
 end
-
-
 
--- lib/sisimai/reason/ratelimited.rb
+++ lib/sisimai/reason/ratelimited.rb
@@ -1,24 +1,26 @@
 module Sisimai
   module Reason
-    # Sisimai::Reason::TooManyConn checks the bounce reason is "toomanyconn" or not. This class is
+    # Sisimai::Reason::RateLimited checks the bounce reason is "ratelimited" or not. This class is
     # called only Sisimai::Reason class.
     #
-    # This is the error that SMTP connection was rejected temporarily due to too many concurrency
-    # connections to the remote server. This reason has added in Sisimai 4.1.26.
+    # This is the error that SMTP connection was rejected temporarily due to too many recipients or
+    # too many concurrency connections to the remote server. This reason has added in Sisimai 4.1.26.
     #
     #   <kijitora@example.ne.jp>: host mx02.example.ne.jp[192.0.1.20] said:
     #     452 4.3.2 Connection rate limit exceeded. (in reply to MAIL FROM command)
-    module TooManyConn
+    module RateLimited
       class << self
         Index = [
           'all available ips are at maximum connection limit',    # SendGrid
           'connection rate limit exceeded',
           'exceeds per-domain connection limit for',
           'has exceeded the max emails per hour ',
+          'mail sent from your IP address has been temporarily rate limited',
+          'please try again slower',
+          'receiving mail at a rate that prevents additional messages from being delivered',
           'throttling failure: daily message quota exceeded',
           'throttling failure: maximum sending rate exceeded',
           'too many connections',
-          'too many connections from your host.', # Microsoft
           'too many concurrent smtp connections', # Microsoft
           'too many errors from your ip',         # Free.fr
           'too many recipients',                  # ntt docomo
@@ -27,7 +29,7 @@ module Sisimai
           'we have already made numerous attempts to deliver this message',
         ].freeze
 
-        def text; return 'toomanyconn'; end
+        def text; return 'ratelimited'; end
         def description; return 'SMTP connection rejected temporarily due to too many concurrency connections to the remote host'; end
 
         # Try to match that the given text and regular expressions
@@ -41,12 +43,12 @@ module Sisimai
 
         # Rejected by domain or address filter ?
         # @param    [Sisimai::Fact] argvs   Object to be detected the reason
-        # @return   [Boolean]               true:  is filtered
-        #                                   false: is not filtered
+        # @return   [Boolean]               true:  is rate limited
+        #                                   false: is not rate limited
         # @see http://www.ietf.org/rfc/rfc2822.txt
         def true(argvs)
-          return true if argvs['reason'] == 'toomanyconn'
-          return true if Sisimai::SMTP::Status.name(argvs['deliverystatus']) == 'toomanyconn'
+          return true if argvs['reason'] == 'ratelimited'
+          return true if Sisimai::SMTP::Status.name(argvs['deliverystatus']) == 'ratelimited'
           return match(argvs['diagnosticcode'].downcase)
         end
 
