6ff702821f183bef274638cace7079cdd343479a
azumakuniyuki <azumakuniyuki@gmail.com>
2026-01-31 18:53:05 +0900

    #407 Merge error message patterns in the Blocked reason

--- lib/sisimai/reason/blocked.rb
+++ lib/sisimai/reason/blocked.rb
@@ -7,93 +7,67 @@ module Sisimai
     # or the parameter of "HELO/EHLO" command. This reason has added in Sisimai 4.0.0.
     module Blocked
       class << self
-        require 'sisimai/string'
-
         Index = [
-          ' said: 550 blocked',
-          '//www.spamcop.net/bl.',
-          'bad sender ip address',
-          'banned sending ip',    # Office365
-          'blacklisted by',
-          'blocked using ',
-          'blocked - see http',
-          'dnsbl:attrbl',
-          'client host rejected: abus detecte gu_eib_02',     # SFR
-          'client host rejected: abus detecte gu_eib_04',     # SFR
-          'client host rejected: may not be mail exchanger',
-          'client host rejected: was not authenticated',      # Microsoft
-          'confirm this mail server',
-          'connection dropped',
-          'connection refused by',
-          'connection reset by peer',
-          'connection was dropped by remote host',
-          'connections not accepted from ip addresses on spamhaus xbl',
-          'currently sending spam see: ',
-          'domain does not exist:',
-          'dynamic/zombied/spam ips blocked',
-          'error: no valid recipients from ',
-          'esmtp not accepting connections',  # icloud.com
-          'extreme bad ip profile',
-          'go away',
-          'helo command rejected:',
-          'host network not allowed',
-          'hosts with dynamic ip',
-          'invalid ip for sending mail of domain',
-          'is in a black list',
-          'is not allowed to send mail from',
-          'mailfrom domain is listed in spamhaus',
-          'no access from mail server',
-          'no matches to nameserver query',
-          'not currently accepting mail from your ip',    # Microsoft
-          'part of their network is on our block list',
-          'please use the smtp server of your isp',
-          'refused - see http',
-          'rejected - multi-blacklist', # junkemailfilter.com
-          'rejected because the sending mta or the sender has not passed validation',
-          'rejecting open proxy', # Sendmail(srvrsmtp.c)
-          'sender ip address rejected',
-          'server access forbidden by your ip ',
-          'service not available, closing transmission channel',
-          'smtp error from remote mail server after initial connection:', # Exim
-          "sorry, that domain isn't in my list of allowed rcpthosts",
-          'sorry, your remotehost looks suspiciously like spammer',
-          'temporarily deferred due to unexpected volume or user complaints',
-          'to submit messages to this e-mail system has been rejected',
-          'too many spams from your ip',  # free.fr
-          'too many unwanted messages have been sent from the following ip address above',
-          'was blocked by ',
-          'we do not accept mail from dynamic ips',   # @mail.ru
-          'you are not allowed to connect',
-          'you are sending spam',
-          'your ip address is listed in the rbl',
-          'your network is temporary blacklisted',
-          'your server requires confirmation',
+          "bad sender ip address",
+          "banned sending ip", # Office365
+          "blacklisted by",
+          "dnsbl:attrbl",
+          "client host rejected: abus detecte gu_eib_02", # SFR
+          "client host rejected: abus detecte gu_eib_04", # SFR
+          "client host rejected: may not be mail exchanger",
+          "connection refused by",
+          "currently sending spam see: ",
+          "domain does not exist:",
+          "domain isn't in my list of allowed rcpthosts",
+          "error: no valid recipients from ",
+          "esmtp not accepting connections", # icloud.com
+          "extreme bad ip profile",
+          "helo command rejected:",
+          "host network not allowed",
+          "invalid ip for sending mail of domain",
+          "is in a black list",
+          "is not allowed to send mail from",
+          "no access from mail server",
+          "part of their network is on our block list",
+          "please use the smtp server of your isp",
+          "rejected because the sending mta or the sender has not passed validation",
+          "rejecting open proxy", # Sendmail(srvrsmtp.c)
+          "sender ip address rejected",
+          "server access forbidden by your ip ",
+          "smtp error from remote mail server after initial connection:", # Exim
+          "you are not allowed to connect",
+          "your ip address is listed in the rbl",
+          "your network is temporary blacklisted",
+          "your remotehost looks suspiciously like spammer",
+          "your server requires confirmation",
         ].freeze
         Pairs = [
-          ['(', '@', ':blocked)'],
-          ['access from ip address ', ' blocked'],
-          ['client host ', ' blocked using'],
-          ['connections will not be accepted from ', " because the ip is in spamhaus's list"],
-          ['dnsbl:rbl ', '>_is_blocked'],
-          ['email blocked by ', '.barracudacentral.org'],
-          ['email blocked by ', 'spamhaus'],
-          ['host ', ' refused to talk to me: ', ' blocked'],
-          ['ip ', ' is blocked by earthlink'],    # Earthlink
-          ['is in an ', 'rbl on '],
-          ['mail server at ', ' is blocked'],
-          ['mail from ',' refused:'],
-          ["mail from "," refused, see ", "orbs"],
-          ['message from ', ' rejected based on blacklist'],
-          ['messages from ', ' temporarily deferred due to user complaints'], # Yahoo!
-          ['server ip ', ' listed as abusive'],
-          ['sorry! your ip address', ' is blocked by rbl'], # junkemailfilter.com
-          ['the domain ', ' is blacklisted'],
-          ['the email ', ' is blacklisted'],
-          ['the ip', ' is blacklisted'],
-          ['veuillez essayer plus tard. service refused, please try later. ', '103'],
-          ['veuillez essayer plus tard. service refused, please try later. ', '510'],
+          ["(", "@", ":blocked)"],
+          ["access from ip address ", " blocked"],
+          ["blocked by ", " dnsbl"],
+          ["client ", " blocked using"],
+          ["connection ", "dropped"],
+          ["client host ", " blocked using"],
+          ["connections will not be accepted from ", " because the ip is in spamhaus's list"],
+          ["dynamic", " ip"],
+          ["email blocked by ", ".barracudacentral.org"],
+          ["email blocked by ", "spamhaus"],
+          ["from ", " ip address"],
+          ["host ", " said: ", "550 blocked"],
+          ["host ", " refused to talk to me: ", " blocked"],
+          ["ip ", " is blocked by earthlink"], # Earthlink
+          ["is in an ", "rbl on "],
+          ["mail server at ", " is blocked"],
+          ["mail from "," refused"],
+          ["message from ", " rejected based on blacklist"],
+          ["messages from ", " temporarily deferred due to user complaints"], # Yahoo!
+          ["server ip ", " listed as abusive"],
+          ["sorry! your ip address", " is blocked by rbl"], # junkemailfilter.com
+          ["the ", " is blacklisted"], # the email, the domain, the ip
+          ["veuillez essayer plus tard. service refused, please try later. ", "103"],
+          ["veuillez essayer plus tard. service refused, please try later. ", "510"],
           ["your access ip", " has been rejected"],
-          ["your sender's ip address is listed at ", '.abuseat.org'],
+          ["your sender's ip address is listed at ", ".abuseat.org"],
         ].freeze
 
         def text; return 'blocked'; end
