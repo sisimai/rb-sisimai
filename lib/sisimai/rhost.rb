6e96e01e042a1237c0b01e83eca889e67d3a3cc4
azumakuniyuki <azumakuniyuki@gmail.com>
2026-02-28 06:56:10 +0900

    #413 List all the Zoho domains: zoho.com.au, zoho.com.cn, zoho.in

--- lib/sisimai/rhost.rb
+++ lib/sisimai/rhost.rb
@@ -24,7 +24,7 @@ module Sisimai
         "Spectrum"    => ["charter.net"],
         "Tencent"     => [".qq.com"],
         "YahooInc"    => [".yahoodns.net"],
-        "Zoho"        => [".zoho.com", ".zoho.eu", ".zoho.jp"],
+        "Zoho"        => [".zoho.com", ".zoho.eu", ".zoho.jp", ".zoho.com.au", ".zoho.com.cn", ".zoho.in"],
       }.freeze
 
       # Returns the rhost class name
