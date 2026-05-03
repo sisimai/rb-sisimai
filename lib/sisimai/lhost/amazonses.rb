81e52a13e03ca5ac7764aa28d61bc516ec80f256
azumakuniyuki <azumakuniyuki@gmail.com>
2026-05-04 06:42:25 +0900

    #428 Reduce excessive calling of Sisimai::String.sweep in Sisimai::Lhost::AmazonSES

--- lib/sisimai/lhost/amazonses.rb
+++ lib/sisimai/lhost/amazonses.rb
@@ -185,7 +185,7 @@ module Sisimai::Lhost
             v["action"]    = "delivered"
             v["date"]      = p["timestamp"]
             v["lhost"]     = Sisimai::RFC1123.find(p["reportingMTA"])
-            v["diagnosis"] = Sisimai::String.sweep(p["smtpResponse"])
+            v["diagnosis"] = p["smtpResponse"]
             v["command"]   = Sisimai::SMTP::Command.find(v["diagnosis"])
             v["status"]    = Sisimai::SMTP::Status.find(v["diagnosis"], "2")
             v["replycode"] = Sisimai::SMTP::Reply.find(v["diagnosis"], "2")
