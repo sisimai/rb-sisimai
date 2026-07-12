b38980af4a2495182bc49452b8c16a56b5361944
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-13 05:32:30 +0900

    #443 Refer constants defined in Sisimai::Eb

--- lib/sisimai/reason/emailtoolarge.rb
+++ lib/sisimai/reason/emailtoolarge.rb
@@ -2,10 +2,11 @@ module Sisimai
   module Reason
     # This is the error that a sent email size is too big for a destination mail server. In many
     # case, There are many attachment files with email, or the file size is too large. Sisimai will
-    # set "emailtoolarge" to the reason of email bounce if the value of Status: field in a bounce
+    # set "EmailTooLarge" to the reason of email bounce if the value of Status: field in a bounce
     # email is "5.2.3" or "5.3.4".
     module EmailTooLarge
       class << self
+        require 'sisimai/eb'
         Index = [
           "exceeds the maximum size ",
           "line limit exceeded",
@@ -20,7 +21,7 @@ module Sisimai
           ["message ", "too", "big"],
         ].freeze
 
-        def text; return 'emailtoolarge'; end
+        def text; return Sisimai::Eb::ReSIZE; end
         def description; return 'Email rejected due to an email size is too big for a destination mail server'; end
 
         # Try to match that the given text and regular expressions
@@ -39,18 +40,18 @@ module Sisimai
         #                                   false: is not big
         # @see http://www.ietf.org/rfc/rfc2822.txt
         def true(argvs)
-          return true if argvs['reason'] == 'emailtoolarge'
+          return true if argvs['reason'] == Sisimai::Eb::ReSIZE
 
           statuscode = argvs['deliverystatus'] || ''
           tempreason = Sisimai::SMTP::Status.name(statuscode)
 
-          # Delivery status code points "emailtoolarge".
+          # Delivery status code points "EmailTooLarge".
           # Status: 5.3.4
           # Diagnostic-Code: SMTP; 552 5.3.4 Error: message file too big
           #
           # Status: 5.2.3
           # Deiagnostic-Code: Message length exceeds administrative limit
-          return true if tempreason == 'emailtoolarge'
+          return true if tempreason == Sisimai::Eb::ReSIZE
           return match(argvs['diagnosticcode'].downcase)
         end
 
