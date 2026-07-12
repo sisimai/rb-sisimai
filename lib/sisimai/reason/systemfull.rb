5904e2924c614aa17297ceac8dc0c853b1a5cbf5
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-13 06:21:41 +0900

    #443 Refer constants defined in Sisimai::Eb

--- lib/sisimai/reason/systemfull.rb
+++ lib/sisimai/reason/systemfull.rb
@@ -1,19 +1,20 @@
 module Sisimai
   module Reason
-    # Sisimai::Reason::SystemFull checks the bounce reason is "systemfull" or not. This class is called
+    # Sisimai::Reason::SystemFull checks the bounce reason is "SystemFull" or not. This class is called
     # only Sisimai::Reason class.
     #
     # This is the error that a destination mail server's disk (or spool) is full. Sisimai will set
-    # systemfull to the reason of email bounce if the value of Status: field in a bounce email is
+    # "SystemFull" to the reason of email bounce if the value of Status: field in a bounce email is
     # "4.3.1" or "5.3.1".
     module SystemFull
       class << self
+        require 'sisimai/eb'
         Index = [
           'exceeded storage allocation',   # MS Exchange
           'mail system full',
         ].freeze
 
-        def text; return 'systemfull'; end
+        def text; return Sisimai::Eb::ReDISK; end
         def description; return "Email rejected due to a destination mail server's disk is full"; end
 
         # Try to match that the given text and regular expressions
