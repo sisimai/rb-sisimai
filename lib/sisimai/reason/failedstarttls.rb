dd51ef5b058b872790df85c02b0dc520c3ca2d8f
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-13 05:34:00 +0900

    #443 Refer constants defined in Sisimai::Eb

--- lib/sisimai/reason/failedstarttls.rb
+++ lib/sisimai/reason/failedstarttls.rb
@@ -1,16 +1,18 @@
 module Sisimai
   module Reason
-    # Sisimai::Reason::FailedSTARTTLS checks the bounce reason is "failedstarttls" or not.
+    # Sisimai::Reason::FailedSTARTTLS checks the bounce reason is "FailedSTARTTLS" or not.
     # This class is called only Sisimai::Reason class.
     module FailedSTARTTLS
       class << self
+        require 'sisimai/eb'
+
         # Email delivery failed due to STARTTLS related problem
         Index = [
           'starttls is required to send mail',
           'tls required but not supported',   # SendGrid:the recipient mailserver does not support TLS or have a valid certificate
         ].freeze
 
-        def text; return 'failedstarttls'; end
+        def text; return Sisimai::Eb::ReTTLS; end
         def description; return "Email delivery failed due to STARTTLS related problem"; end
 
         # Try to match that the given text and regular expressions
@@ -28,7 +30,7 @@ module Sisimai
         #                                   false: Not FailedSTARTTLS
         # @see http://www.ietf.org/rfc/rfc2822.txt
         def true(argvs)
-          return true if argvs["reason"] == "failedstarttls" || argvs["command"] == "STARTTLS"
+          return true if argvs["reason"] == Sisimai::Eb::ReTTLS || argvs["command"] == "STARTTLS"
           return true if [523, 524, 538].index(argvs["replycode"].to_i)
           return match(argvs["diagnosticcode"].downcase)
         end
