4f7817056bfb512381e49ae4f4c83104951becd2
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-13 05:17:46 +0900

    #443 Refer constants defined in Sisimai::Eb

--- lib/sisimai/reason/authfailure.rb
+++ lib/sisimai/reason/authfailure.rb
@@ -1,6 +1,6 @@
 module Sisimai
   module Reason
-    # Sisimai::Reason::AuthFailure checks the bounce reason is "authfailure" or not. This class is
+    # Sisimai::Reason::AuthFailure checks the bounce reason is "AuthFailure" or not. This class is
     # called only Sisimai::Reason class.
     #
     # This is the error that an authenticaion failure related to SPF, DKIM, or DMARC was detected
@@ -12,6 +12,7 @@ module Sisimai
     # Diagnostic-Code: smtp; 550 5.7.1 Email rejected per DMARC policy for example.org
     module AuthFailure
       class << self
+        require 'sisimai/eb'
         Index = [
           "//spf.pobox.com",
           "5322.From address doesn't meet the authentication requirements",
@@ -27,7 +28,7 @@ module Sisimai
           ["is not allowed to send ", " spf "],
         ].freeze
 
-        def text; return 'authfailure'; end
+        def text; return Sisimai::Eb::ReAUTH; end
         def description; return 'Email rejected due to SPF, DKIM, DMARC failure'; end
 
         # Try to match that the given text and regular expressions
@@ -40,14 +41,14 @@ module Sisimai
           return false
         end
 
-        # The bounce reason is "authfailure" or not
+        # The bounce reason is "AuthFailure" or not
         # @param    [Sisimai::Fact] argvs Object to be detected the reason
         # @return   [Boolean]             true: is AuthFailure, false: is not AuthFailure
         # @see      http://www.ietf.org/rfc/rfc2822.txt
         def true(argvs)
           return false if argvs['deliverystatus'].empty?
-          return true  if argvs['reason'] == 'authfailure'
-          return true  if Sisimai::SMTP::Status.name(argvs['deliverystatus']) == 'authfailure'
+          return true  if argvs['reason'] == Sisimai::Eb::ReAUTH
+          return true  if Sisimai::SMTP::Status.name(argvs['deliverystatus']) == Sisimai::Eb::ReAUTH
           return match(argvs['diagnosticcode'].downcase)
         end
 
