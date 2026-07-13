d6707f393c1a6cc5a65a64153843ec4f65736106
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-14 04:59:45 +0900

    #443 Refer constants defined in Sisimai::Eb

--- lib/sisimai/rhost/cox.rb
+++ lib/sisimai/rhost/cox.rb
@@ -5,12 +5,13 @@ module Sisimai
     # called only Sisimai::Fact class.
     module Cox
       class << self
+        require 'sisimai/eb'
         ErrorCodes = {
           # CXBL
           # - The sending IP address has been blocked by Cox due to exhibiting spam-like behavior.
           # - Send an email request to Cox to ask for a sending IP address be unblocked.
           #   Note: Cox has sole discretion whether to unblock the sending IP address.
-          'CXBL' => 'blocked',
+          'CXBL' => Sisimai::Eb::ReBLOC,
 
           # CXDNS
           # - There was an issue with the connecting IP address Domain Name System (DNS).
@@ -18,66 +19,66 @@ module Sisimai
           #   - Confirm the IP address that sends your email.
           #   - Check the rDNS of that IP address. If it passes, then wait 24 hours and try resending
           #     your email.
-          'CXDNS' => 'requireptr',
+          'CXDNS' => Sisimai::Eb::ReQPTR,
 
           # CXSNDR
           # - There was a problem with the sender's domain.
           # - Your email failed authentication checks against your sending domain's SPF, DomainKeys,
           #   or DKIM policy.
-          'CXSNDR' => 'authfailure',
+          'CXSNDR' => Sisimai::Eb::ReAUTH,
 
           # CXSMTP
           # - There was a violation of SMTP protocol.
           # - Your email wasn't delivered because Cox was unable to verify that it came from a
           #   legitimate email sender.
-          'CXSMTP' => 'rejected',
+          'CXSMTP' => Sisimai::Eb::ReFROM,
 
           # CXCNCT
           # - There was a connection issue from the IP address.
           # - There is a limit to the number of concurrent SMTP connections per IP address to
           #   protect the systems against attack. Ensure that the sending email server is not
           #   opening more than 10 concurrent connections to avoid reaching this limit.
-          'CXCNCT' => 'ratelimited',
+          'CXCNCT' => Sisimai::Eb::ReRATE,
 
           # CXMXRT
           #   - The sender has sent email to too many recipients and needs to wait before sending
           #     more email.
           #   - The email sender has exceeded the maximum number of sent email allowed.
-          'CXMXRT' => 'ratelimited', 
+          'CXMXRT' => Sisimai::Eb::ReRATE,
 
           # CDRBL
           # - The sending IP address has been temporarily blocked by Cox due to exhibiting spam-like
           #   behavior.
           # - The block duration varies depending on reputation and other factors, but will not exceed
           #   24 hours. Inspect email traffic for potential spam, and retry email delivery.
-          'CDRBL' => 'blocked',
+          'CDRBL' => Sisimai::Eb::ReBLOC,
 
-          'CXTHRT'    => 'securityerror', # Email sending limited due to suspicious account activity.
-          'CXMJ'      => 'securityerror', # Email sending blocked due to suspicious account activity on primary Cox account.
-          'IPBL0001'  => 'blocked',       # The sending IP address is listed in the Spamhaus Zen DNSBL.
-          'IPBL0010'  => 'blocked',       # The sending IP is listed in the Return Path DNSBL.
-          'IPBL0100'  => 'blocked',       # The sending IP is listed in the Invaluement ivmSIP DNSBL.
-          'IPBL0011'  => 'blocked',       # The sending IP is in the Spamhaus Zen and Return Path DNSBLs.
-          'IPBL0101'  => 'blocked',       # The sending IP is in the Spamhaus Zen and Invaluement ivmSIP DNSBLs.
-          'IPBL0110'  => 'blocked',       # The sending IP is in the Return Path and Invaluement ivmSIP DNSBLs.
-          'IPBL0111'  => 'blocked',       # The sending IP is in the Spamhaus Zen, Return Path and Invaluement ivmSIP DNSBLs.
-          'IPBL1000'  => 'blocked',       # The sending IP address is listed on a CSI blacklist.
-          'IPBL1001'  => 'blocked',       # The sending IP is listed in the Cloudmark CSI and Spamhaus Zen DNSBLs.
-          'IPBL1010'  => 'blocked',       # The sending IP is listed in the Cloudmark CSI and Return Path DNSBLs.
-          'IPBL1011'  => 'blocked',       # The sending IP is in the Cloudmark CSI, Spamhaus Zen and Return Path DNSBLs.
-          'IPBL1100'  => 'blocked',       # The sending IP is listed in the Cloudmark CSI and Invaluement ivmSIP DNSBLs.
-          'IPBL1101'  => 'blocked',       # The sending IP is in the Cloudmark CSI, Spamhaus Zen and Invaluement IVMsip DNSBLs.
-          'IPBL1110'  => 'blocked',       # The sending IP is in the Cloudmark CSI, Return Path and Invaluement ivmSIP DNSBLs.
-          'IPBL1111'  => 'blocked',       # The sending IP is in the Cloudmark CSI, Spamhaus Zen, Return Path and Invaluement ivmSIP DNSBLs.
-          'IPBL00001' => 'blocked',       # The sending IP address is listed on a Spamhaus blacklist.
+          'CXTHRT'    => Sisimai::Eb::ReSAFE, # Email sending limited due to suspicious account activity.
+          'CXMJ'      => Sisimai::Eb::ReSAFE, # Email sending blocked due to suspicious account activity on primary Cox account.
+          'IPBL0001'  => Sisimai::Eb::ReBLOC, # The sending IP address is listed in the Spamhaus Zen DNSBL.
+          'IPBL0010'  => Sisimai::Eb::ReBLOC, # The sending IP is listed in the Return Path DNSBL.
+          'IPBL0100'  => Sisimai::Eb::ReBLOC, # The sending IP is listed in the Invaluement ivmSIP DNSBL.
+          'IPBL0011'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Spamhaus Zen and Return Path DNSBLs.
+          'IPBL0101'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Spamhaus Zen and Invaluement ivmSIP DNSBLs.
+          'IPBL0110'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Return Path and Invaluement ivmSIP DNSBLs.
+          'IPBL0111'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Spamhaus Zen, Return Path and Invaluement ivmSIP DNSBLs.
+          'IPBL1000'  => Sisimai::Eb::ReBLOC, # The sending IP address is listed on a CSI blacklist.
+          'IPBL1001'  => Sisimai::Eb::ReBLOC, # The sending IP is listed in the Cloudmark CSI and Spamhaus Zen DNSBLs.
+          'IPBL1010'  => Sisimai::Eb::ReBLOC, # The sending IP is listed in the Cloudmark CSI and Return Path DNSBLs.
+          'IPBL1011'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Cloudmark CSI, Spamhaus Zen and Return Path DNSBLs.
+          'IPBL1100'  => Sisimai::Eb::ReBLOC, # The sending IP is listed in the Cloudmark CSI and Invaluement ivmSIP DNSBLs.
+          'IPBL1101'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Cloudmark CSI, Spamhaus Zen and Invaluement IVMsip DNSBLs.
+          'IPBL1110'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Cloudmark CSI, Return Path and Invaluement ivmSIP DNSBLs.
+          'IPBL1111'  => Sisimai::Eb::ReBLOC, # The sending IP is in the Cloudmark CSI, Spamhaus Zen, Return Path and Invaluement ivmSIP DNSBLs.
+          'IPBL00001' => Sisimai::Eb::ReBLOC, # The sending IP address is listed on a Spamhaus blacklist.
 
-          'URLBL011'  => 'spamdetected',  # A URL within the body of the message was found on blocklists SURBL and Spamhaus DBL.
-          'URLBL101'  => 'spamdetected',  # A URL within the body of the message was found on blocklists SURBL and ivmURI.
-          'URLBL110'  => 'spamdetected',  # A URL within the body of the message was found on blocklists Spamhaus DBL and ivmURI.
-          'URLBL1001' => 'spamdetected',  # The URL is listed on a Spamhaus blacklist.
+          'URLBL011'  => Sisimai::Eb::ReSPAM, # A URL within the body of the message was found on blocklists SURBL and Spamhaus DBL.
+          'URLBL101'  => Sisimai::Eb::ReSPAM, # A URL within the body of the message was found on blocklists SURBL and ivmURI.
+          'URLBL110'  => Sisimai::Eb::ReSPAM, # A URL within the body of the message was found on blocklists Spamhaus DBL and ivmURI.
+          'URLBL1001' => Sisimai::Eb::ReSPAM, # The URL is listed on a Spamhaus blacklist.
         }.freeze
         MessagesOf = {
-          'blocked' => [
+          Sisimai::Eb::ReBLOC => [
             # - An email client has repeatedly sent bad commands or invalid passwords resulting in
             #   a three-hour block of the client's IP address.
             # - The sending IP address has exceeded the threshold of invalid recipients and has
@@ -87,18 +88,18 @@ module Sisimai
             'cox too many bad commands from',
             'too many invalid recipients',
           ],
-          'contenterror' => [
+          Sisimai::Eb::ReBODY => [
             # - The message has been rejected because it contains an attachment with one of the
             #   following prohibited file types, which commonly contain viruses: .shb, .shs, .vbe,
             #   .vbs, .wsc, .wsf, .wsh, .pif, .msc, .msi, .msp, .reg, .sct, .bat, .chm, .isp, .cpl,
             #   .js, .jse, .scr, .exe.
             'attachment extension is forbidden',
           ],
-          'policyviolation' => [
+          Sisimai::Eb::ReWONT => [
             # - The sending server has attempted to communicate too soon within the SMTP transaction
             'esmtp no data before greeting',
           ],
-          'ratelimited' => [
+          Sisimai::Eb::ReWONT => [
             # - The sending IP address has exceeded the five maximum concurrent connection limit.
             # - The SMTP connection has exceeded the 100 email message threshold and was disconnected.
             # - The sending IP address has exceeded one of these rate limits and has been temporarily
@@ -107,22 +108,22 @@ module Sisimai
             'requested action aborted: try again later',
             'message threshold exceeded',
           ],
-          'rejected' => [
+          Sisimai::Eb::ReFROM => [
             # Cox requires that all sender domains resolve to a valid MX or A-record within DNS.
             'sender rejected',
           ],
-          'requireptr' => [
+          Sisimai::Eb::ReQPTR => [
             # - The reverse DNS check of the sending server IP address has failed.
             # - Cox requires that all connecting email servers contain valid reverse DNS PTR records.
             'dns check failure - try again later',
             'rejected - no rdns',
           ],
-          'systemerror' => [
+          Sisimai::Eb::RePROC => [
             # - Our systems are experiencing an issue which is causing a temporary inability to
             #   accept new email.
             'esmtp server temporarily not available',
           ],
-          'userunknown' => [
+          Sisimai::Eb::ReUSER => [
             # - The intended recipient is not a valid Cox Email account.
             'recipient rejected',
           ],
