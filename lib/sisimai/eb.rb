fcd56f5b021401e4a479870452991b3a95245257
azumakuniyuki <azumakuniyuki@gmail.com>
2026-07-16 04:52:47 +0900

    #447 Define SMTP commands as a constant

--- lib/sisimai/eb.rb
+++ lib/sisimai/eb.rb
@@ -1,6 +1,11 @@
 module Sisimai
   # Sisimai::Eb - Package "eb" provides constants for the email bounce.
   module Eb
+    #       _       ______
+    #   ___| |__   / /  _ \ ___  __ _ ___  ___  _ __
+    #  / _ \ '_ \ / /| |_) / _ \/ _` / __|/ _ \| '_ \
+    # |  __/ |_) / / |  _ <  __/ (_| \__ \ (_) | | | |
+    #  \___|_.__/_/  |_| \_\___|\__,_|___/\___/|_| |_|
     # bounce reason names
     ReAUTH = "AuthFailure"
     ReFAMA = "BadReputation"
@@ -36,6 +41,28 @@ module Sisimai
     ReUSER = "UserUnknown"
     ReAWAY = "Vacation"
     ReEXEC = "VirusDetected"
+
+    #       _       ______                                          _
+    #   ___| |__   / / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| |
+    #  / _ \ '_ \ / / |   / _ \| '_ ` _ \| '_ ` _ \ / _` | '_ \ / _` |
+    # |  __/ |_) / /| |__| (_) | | | | | | | | | | | (_| | | | | (_| |
+    #  \___|_.__/_/  \____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|
+    CeHELO = "HELO"
+    CeEHLO = "EHLO"
+    CeMAIL = "MAIL"
+    CeRCPT = "RCPT"
+    CeDATA = "DATA"
+    CeQUIT = "QUIT"
+    CeRSET = "RSET"
+    CeNOOP = "NOOP"
+    CeVRFY = "VRFY"
+    CeETRN = "ETRN"
+    CeEXPN = "EXPN"
+    CeHELP = "HELP"
+    CeAUTH = "AUTH"
+    CeTTLS = "STARTTLS"
+    CeXFWD = "XFORWARD"
+    CeCONN = "CONN" # CONN is a pseudo SMTP command used only in Sisimai
   end
 end
 
