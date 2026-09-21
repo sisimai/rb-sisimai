e10c0d2b2eae4d13bd8566333473e9dd6a950465
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-22 05:38:46 +0900

    #463 Returns nil if Sisimai::RFC5322.part returned nil

--- lib/sisimai/lhost/opensmtpd.rb
+++ lib/sisimai/lhost/opensmtpd.rb
@@ -47,7 +47,7 @@ module Sisimai::Lhost
         return nil if mhead['received'].none? { |a| a.include?(' (OpenSMTPD) with ') }
 
         dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries)
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
         bodyslices = emailparts[0].split("\n")
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
