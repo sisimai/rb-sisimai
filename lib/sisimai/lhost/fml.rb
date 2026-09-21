ddebb6c581ef87277fb21f78d5cb350db7720027
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-22 05:20:24 +0900

    #463 Returns nil if Sisimai::RFC5322.part returned nil

--- lib/sisimai/lhost/fml.rb
+++ lib/sisimai/lhost/fml.rb
@@ -36,7 +36,7 @@ module Sisimai::Lhost
         return nil if mhead['message-id'].index('.FML') < 2
 
         dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries)
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
         bodyslices = emailparts[0].split("\n")
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
 
