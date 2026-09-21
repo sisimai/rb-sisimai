b32e2b637525481e05e4a686123b18e664b56103
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-22 05:39:13 +0900

    #463 Returns nil if Sisimai::RFC5322.part returned nil

--- lib/sisimai/lhost/postfix.rb
+++ lib/sisimai/lhost/postfix.rb
@@ -44,7 +44,7 @@ module Sisimai::Lhost
 
         permessage = {}     # (Hash) Store values of each Per-Message field
         dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries)
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
         bodyslices = emailparts[0].split("\n")
         readslices = ['']
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
