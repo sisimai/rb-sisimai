8d14fa7ccb822eb1a51f2a839f5ffa48dd44fb69
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-23 04:48:36 +0900

    Follow #463 Change the order of declarations(emailparts)

--- lib/sisimai/lhost/exchange2003.rb
+++ lib/sisimai/lhost/exchange2003.rb
@@ -84,8 +84,8 @@ module Sisimai::Lhost
         end
         return nil if match == 0
 
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         bodyslices = emailparts[0].split("\n")
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
