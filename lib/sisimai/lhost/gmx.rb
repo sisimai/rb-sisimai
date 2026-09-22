503bc37a265aa480cecd87730f2ca0b8493a0995
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-23 04:55:56 +0900

    Follow #463 Change the order of declarations(emailparts)

--- lib/sisimai/lhost/gmx.rb
+++ lib/sisimai/lhost/gmx.rb
@@ -22,8 +22,8 @@ module Sisimai::Lhost
         return nil if mhead['x-gmx-antispam'].nil?
 
         require 'sisimai/smtp/command'
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         bodyslices = emailparts[0].split("\n")
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
