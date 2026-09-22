fb99fca930b7cd74758c9bac20b62402be24d1e7
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-23 04:38:40 +0900

    Follow #463 Change the order of declarations(emailparts)

--- lib/sisimai/lhost/activehunter.rb
+++ lib/sisimai/lhost/activehunter.rb
@@ -20,8 +20,8 @@ module Sisimai::Lhost
         # :subject => %r/FAILURE NOTICE :/,
         return nil if mhead['x-ahmailid'].nil?
 
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         bodyslices = emailparts[0].split("\n")
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
