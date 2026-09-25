a4a70cc36a2b55587793ddd1cab38c6780b8961e
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-26 04:32:07 +0900

    Follow #463 again, Check that the first element of emailparts is empty or not

--- lib/sisimai/lhost/activehunter.rb
+++ lib/sisimai/lhost/activehunter.rb
@@ -20,9 +20,9 @@ module Sisimai::Lhost
         # :subject => %r/FAILURE NOTICE :/,
         return nil if mhead['x-ahmailid'].nil?
 
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil? || emailparts[0].empty?
         bodyslices = emailparts[0].split("\n")
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
 
