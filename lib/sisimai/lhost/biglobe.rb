ef5a1b63ae643be80940fbbf405feac2f0f5689a
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-26 04:35:38 +0900

    Follow #463 again, Check that the first element of emailparts is empty or not

--- lib/sisimai/lhost/biglobe.rb
+++ lib/sisimai/lhost/biglobe.rb
@@ -22,9 +22,9 @@ module Sisimai::Lhost
         return nil if %w[biglobe inacatv tmtv ttv].none? { |a| mhead['from'].include?('@' + a + '.ne.jp') }
         return nil if mhead['subject'].start_with?('Returned mail:') == false
 
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil? || emailparts[0].empty?
         bodyslices = emailparts[0].split("\n")
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
 
