ed1250cb706a76c171f689c63856f37c91156d5b
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-26 04:49:38 +0900

    Follow #463 again, Check that the first element of emailparts is empty or not

--- lib/sisimai/lhost/exchange2007.rb
+++ lib/sisimai/lhost/exchange2007.rb
@@ -74,9 +74,9 @@ module Sisimai::Lhost
         require "sisimai/rfc1123"
         require "sisimai/smtp/reply"
         require "sisimai/smtp/status"
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil? || emailparts[0].empty?
         bodyslices = emailparts[0].split("\n")
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
 
