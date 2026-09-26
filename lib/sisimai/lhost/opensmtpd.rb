5f4361443d1a65c830076e77b611b7e2c26b9b65
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-26 09:39:46 +0900

    Follow #463 again, Check that the first element of emailparts is empty or not

--- lib/sisimai/lhost/opensmtpd.rb
+++ lib/sisimai/lhost/opensmtpd.rb
@@ -46,9 +46,9 @@ module Sisimai::Lhost
         return nil if mhead['from'].start_with?('Mailer Daemon <') == false
         return nil if mhead['received'].none? { |a| a.include?(' (OpenSMTPD) with ') }
 
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil? || emailparts[0].empty?
         bodyslices = emailparts[0].split("\n")
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
 
