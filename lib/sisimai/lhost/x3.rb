1c1f68c4edeaa878409632fd6503ff9cbcede2f3
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-26 10:01:28 +0900

    Follow #463 again, Check that the first element of emailparts is empty or not

--- lib/sisimai/lhost/x3.rb
+++ lib/sisimai/lhost/x3.rb
@@ -19,9 +19,9 @@ module Sisimai::Lhost
         return nil if mhead['from'].start_with?('Mail Delivery System') == false
 
         require 'sisimai/smtp/command'
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil? || emailparts[0].empty?
         bodyslices = emailparts[0].split("\n")
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
         v = nil
