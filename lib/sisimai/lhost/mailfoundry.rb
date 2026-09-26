99c2d735a6ec479eef69f835948ef6a87cde57be
azumakuniyuki <azumakuniyuki@gmail.com>
2026-09-26 09:24:47 +0900

    Follow #463 again, Check that the first element of emailparts is empty or not

--- lib/sisimai/lhost/mailfoundry.rb
+++ lib/sisimai/lhost/mailfoundry.rb
@@ -21,9 +21,9 @@ module Sisimai::Lhost
         return nil if mhead['subject'] != 'Message delivery has failed'
         return nil if mhead['received'].none? { |a| a.include?('(MAILFOUNDRY) id ') }
 
-        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil?
-        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
+        emailparts = Sisimai::RFC5322.part(mbody, Boundaries); return nil if emailparts.nil? || emailparts[0].empty?
         bodyslices = emailparts[0].split("\n")
+        dscontents = [Sisimai::Lhost.DELIVERYSTATUS]; v = nil
         readcursor = 0      # (Integer) Points the current cursor position
         recipients = 0      # (Integer) The number of 'Final-Recipient' header
 
